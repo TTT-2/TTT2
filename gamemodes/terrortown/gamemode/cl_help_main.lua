6f82412c7c66a4ef0eb4f9a97d4529dc06037654
Alf21 <4lf-mueller@gmx.de>
2018-11-21 17:40:37 +0100

    Added fix https://github.com/Facepunch/garrysmod/pull/1537/files (prevent help window opening multiple times)

--- gamemodes/terrortown/gamemode/cl_help_main.lua
+++ gamemodes/terrortown/gamemode/cl_help_main.lua
@@ -10,12 +10,16 @@ CreateClientConVar("ttt_avoid_detective", "0", true, true)
 
 HELPSCRN = {}
 
+local dframe
+
 function HELPSCRN:Show()
+	if IsValid(dframe) then return end
+
 	local client = LocalPlayer()
 	local margin = 15
 	local w, h = 630, 470
 
-	local dframe = vgui.Create("DFrame")
+	dframe = vgui.Create("DFrame")
 	dframe:SetSize(w, h)
 	dframe:Center()
 	dframe:SetTitle(GetTranslation("help_title"))
