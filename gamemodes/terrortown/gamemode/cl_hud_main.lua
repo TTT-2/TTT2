e68a7d24316ca129e1e669fb0e3fcd5d26f3169b
Alf21 <4lf-mueller@gmx.de>
2018-12-04 12:45:20 +0100

    Added own Item Info functions

--- gamemodes/terrortown/gamemode/cl_hud_main.lua
+++ gamemodes/terrortown/gamemode/cl_hud_main.lua
@@ -344,6 +344,47 @@ local function InfoPaint(client)
 	end
 end
 
+-- item info
+local HUD_ITEMS = {}
+local defaultY = ScrH() * 0.5 + 20
+
+function AddHUDItem(id, material)
+	HUD_ITEMS[#HUD_ITEMS + 1] = {id = id, material = material}
+end
+
+function getYCoordinate(currentPerkID)
+	local amount, i, perk = 0, 1
+
+	while i < currentPerkID do
+		perk = GetEquipmentItemByID(i)
+
+		if istable(perk) and perk.hud and LocalPlayer():HasEquipmentItem(perk.id) then
+			amount = amount + 1
+		end
+
+		i = i * 2
+	end
+
+	return defaultY - 80 * amount
+end
+
+local function ItemInfo(client)
+	for k, itemData in ipairs(HUD_ITEMS) do
+		if client:HasEquipmentItem(itemData.id) then
+			local y = itemData.y
+
+			if not y then
+				y = client:HasEquipmentItem(itemData.id) and getYCoordinate(itemData.id) or defaultY
+				HUD_ITEMS[k].y = y
+			end
+
+			surface.SetMaterial(itemData.material)
+			surface.SetDrawColor(255, 255, 255, 255)
+			surface.DrawTexturedRect(20, y, 64, 64)
+		end
+	end
+end
+
 -- Paints player status HUD element in the bottom left
 function GM:HUDPaint()
 	local client = LocalPlayer()
@@ -391,6 +432,11 @@ function GM:HUDPaint()
 	-- Draw bottom left info panel
 	if hook.Call("HUDShouldDraw", GAMEMODE, "TTTInfoPanel") then
 		InfoPaint(client)
+	end
+
+	-- Draw owned Item info
+	if hook.Call("HUDShouldDraw", GAMEMODE, "TTT2ItemInfo") then
+		ItemInfo(client)
 	end
 end
 
