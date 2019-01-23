0a727a11b3cfec22d53a92c2e3a2eab966f26eaf
Alf21 <4lf-mueller@gmx.de>
2019-01-23 19:37:31 +0100

    Added support for item hud display

--- gamemodes/terrortown/gamemode/cl_hud_item_main.lua
+++ gamemodes/terrortown/gamemode/cl_hud_item_main.lua
@@ -1,43 +1,18 @@
 -- item info
-local HUD_ITEMS = {}
 local defaultY = ScrH() * 0.5 + 20
 
 local ipairs = ipairs
 
-function AddHUDItem(id, material)
-	HUD_ITEMS[#HUD_ITEMS + 1] = {id = id, material = material}
-end
-
-local function getYCoordinate(currentPerkID)
-	local amount, i, perk = 0, 1
-	local client = LocalPlayer()
-
-	while i < currentPerkID do
-		perk = GetEquipmentItemByID(i)
-
-		if istable(perk) and perk.hud and client:HasEquipmentItem(perk.id) then
-			amount = amount + 1
-		end
-
-		i = i * 2
-	end
-
-	return defaultY - 80 * amount
-end
-
 local function ItemInfo(client)
-	for k, itemData in ipairs(HUD_ITEMS) do
-		if client:HasEquipmentItem(itemData.id) then
-			local y = itemData.y
+	local y = defaultY
 
-			if not y then
-				y = client:HasEquipmentItem(itemData.id) and getYCoordinate(itemData.id) or defaultY
-				HUD_ITEMS[k].y = y
-			end
-
-			surface.SetMaterial(itemData.material)
+	for _, item in ipairs(items.GetList()) do
+		if item.icon and client:HasEquipmentItem(item.id) then
+			surface.SetMaterial(item.icon)
 			surface.SetDrawColor(255, 255, 255, 255)
 			surface.DrawTexturedRect(20, y, 64, 64)
+
+			y = y - 80
 		end
 	end
 end
@@ -49,5 +24,25 @@ hook.Add("HUDPaint", "TTT2ItemHud", function()
 	-- Draw owned Item info
 	if client:Alive() and client:Team() ~= TEAM_SPEC and hook.Call("HUDShouldDraw", GAMEMODE, "TTT2ItemInfo") then
 		ItemInfo(client)
+	end
+end)
+
+hook.Add("TTTBodySearchPopulate", "TTT2InitHUDItems", function(search, raw)
+	for _, item in ipairs(items.GetList()) do
+		if not raw["eq_" .. item.id] then return end
+
+		local highest = 0
+
+		for _, v in pairs(search) do
+			highest = math.max(highest, v.p)
+		end
+
+		search["eq_" .. item.id] = {img = item.material, text = item.desc, p = highest + 1}
+	end
+end)
+
+hook.Add("TTTBodySearchEquipment", "TTTSpeedrunCorpseIcon", function(search, eq)
+	for _, item in ipairs(items.GetList()) do
+		search["eq_" .. item.id] = table.HasValue(eq, item.id)
 	end
 end)
---
+++ lua/autorun/ttt2_modules.lua
@@ -0,0 +1,2 @@
+-- include modules
+require("items")
