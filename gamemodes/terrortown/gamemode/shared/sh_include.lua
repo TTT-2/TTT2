f0859b34bdfd9d6ae97ae398003e6409b72a73c4
Alf21 <4lf-mueller@gmx.de>
2019-01-31 19:13:41 +0100

    fix

--- gamemodes/terrortown/gamemode/shared/sh_include.lua
+++ gamemodes/terrortown/gamemode/shared/sh_include.lua
@@ -45,7 +45,7 @@ TTTFiles = {
 	vgui__cl_simpleicon = {file = "vgui/cl_simpleicon.lua", on = "client"},
 	vgui__cl_shopeditor_buttons = {file = "vgui/cl_shopeditor_buttons.lua", on = "client"},
 	vgui__cl_shopeditor_slider = {file = "vgui/cl_shopeditor_slider.lua", on = "client"},
-	vgui__cl_simpleclickicon = {file = "vguicl_/simpleclickicon.lua", on = "client"},
+	vgui__cl_simpleclickicon = {file = "vgui/cl_simpleclickicon.lua", on = "client"},
 	vgui__cl_progressbar = {file = "vgui/cl_progressbar.lua", on = "client"},
 	vgui__cl_scrolllabel = {file = "vgui/cl_scrolllabel.lua", on = "client"},
 	vgui__cl_sb_main = {file = "vgui/cl_sb_main.lua", on = "client"},
