3267b1cd5017bd2872dbaa408bd0d01647545c2f
Alf21 <4lf-mueller@gmx.de>
2019-02-02 17:20:31 +0100

    check element real time reloading

--- gamemodes/terrortown/gamemode/shared/hud_elements/base_elements/pure_skin_element.lua
+++ gamemodes/terrortown/gamemode/shared/hud_elements/base_elements/pure_skin_element.lua
@@ -1,5 +1,8 @@
 if CLIENT then
 	function HUDELEMENT:DrawBg(x, y, w, h, c)
+		surface.SetDrawColor(100, 100, 100, 50)
+		surface.DrawRect(x - 1, y - 1, w + 1, h + 1)
+
 		surface.SetDrawColor(clr(c))
 		surface.DrawRect(x, y, w, h)
 	end
