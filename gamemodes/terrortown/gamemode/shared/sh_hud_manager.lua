a43487d69dfc6892e1dd27ace231946e99d7dcc1
Alf21 <4lf-mueller@gmx.de>
2019-02-02 14:44:05 +0100

    always include the other files

--- gamemodes/terrortown/gamemode/shared/sh_hud_manager.lua
+++ gamemodes/terrortown/gamemode/shared/sh_hud_manager.lua
@@ -1,88 +1,88 @@
-if HUDManager then return end
+if not HUDManager then
+	require("hudelements")
+	require("huds")
 
-require("hudelements")
-require("huds")
+	HUDManager = {}
+	HUDManager.defaultHUD = "old_ttt"
 
-HUDManager = {}
-HUDManager.defaultHUD = "old_ttt"
+	-----------------------------------------
+	-- now load the HUDs and the HUD Elements
+	-----------------------------------------
 
------------------------------------------
--- now load the HUDs and the HUD Elements
------------------------------------------
+	local autoload = {
+		"hud_elements",
+		"huds"
+	}
 
-local autoload = {
-	"hud_elements",
-	"huds"
-}
+	for _, savedFolder in ipairs(autoload) do
+		local pathBase = "terrortown/gamemode/shared/" .. savedFolder .. "/"
 
-for _, savedFolder in ipairs(autoload) do
-	local pathBase = "terrortown/gamemode/shared/" .. savedFolder .. "/"
+		local hudelementsFiles = file.Find(pathBase .. "*.lua", "LUA")
 
-	local hudelementsFiles = file.Find(pathBase .. "*.lua", "LUA")
-
-	-- include HUD Elements files
-	for _, fl in ipairs(hudelementsFiles) do
-		if savedFolder == "hud_elements" then
-			HUDELEMENT = {}
-		elseif savedFolder == "huds" then
-			HUD = {}
-		end
-
-		if SERVER then
-			AddCSLuaFile(pathBase .. fl)
-		end
-
-		include(pathBase .. fl)
-
-		local cls = string.sub(fl, 0, #fl - 4)
-
-		if savedFolder == "hud_elements" then
-			hudelements.Register(HUDELEMENT, cls)
-
-			HUDELEMENT = nil
-		elseif savedFolder == "huds" then
-			huds.Register(HUD, cls)
-
-			HUD = nil
-		end
-	end
-
-	-- include HUD Elements folders
-	local _, subFolders = file.Find(pathBase .. "*", "LUA")
-
-	for _, folder in ipairs(subFolders) do
-		if savedFolder == "hud_elements" then
-			HUDELEMENT = {}
-		elseif savedFolder == "huds" then
-			HUD = {}
-		end
-
-		local subFiles = file.Find(pathBase .. folder .. "/*.lua", "LUA")
-
-		for _, fl in ipairs(subFiles) do
-			local filename = pathBase .. folder .. "/" .. fl
+		-- include HUD Elements files
+		for _, fl in ipairs(hudelementsFiles) do
+			if savedFolder == "hud_elements" then
+				HUDELEMENT = {}
+			elseif savedFolder == "huds" then
+				HUD = {}
+			end
 
 			if SERVER then
-				AddCSLuaFile(filename)
+				AddCSLuaFile(pathBase .. fl)
 			end
 
-			if CLIENT and fl == "cl_init.lua" then
-				include(filename)
-			elseif SERVER and fl == "init.lua" then
-				include(filename)
-			elseif fl == "shared.lua" then
-				include(filename)
+			include(pathBase .. fl)
+
+			local cls = string.sub(fl, 0, #fl - 4)
+
+			if savedFolder == "hud_elements" then
+				hudelements.Register(HUDELEMENT, cls)
+
+				HUDELEMENT = nil
+			elseif savedFolder == "huds" then
+				huds.Register(HUD, cls)
+
+				HUD = nil
 			end
 		end
 
-		if savedFolder == "hud_elements" then
-			hudelements.Register(HUDELEMENT, folder)
+		-- include HUD Elements folders
+		local _, subFolders = file.Find(pathBase .. "*", "LUA")
 
-			HUDELEMENT = nil
-		elseif savedFolder == "huds" then
-			huds.Register(HUD, folder)
+		for _, folder in ipairs(subFolders) do
+			if savedFolder == "hud_elements" then
+				HUDELEMENT = {}
+			elseif savedFolder == "huds" then
+				HUD = {}
+			end
 
-			HUD = nil
+			local subFiles = file.Find(pathBase .. folder .. "/*.lua", "LUA")
+
+			for _, fl in ipairs(subFiles) do
+				local filename = pathBase .. folder .. "/" .. fl
+
+				if SERVER then
+					AddCSLuaFile(filename)
+				end
+
+				if CLIENT and fl == "cl_init.lua" then
+					include(filename)
+				elseif SERVER and fl == "init.lua" then
+					include(filename)
+				elseif fl == "shared.lua" then
+					include(filename)
+				end
+			end
+
+			if savedFolder == "hud_elements" then
+				hudelements.Register(HUDELEMENT, folder)
+
+				HUDELEMENT = nil
+			elseif savedFolder == "huds" then
+				huds.Register(HUD, folder)
+
+				HUD = nil
+			end
 		end
 	end
 end
