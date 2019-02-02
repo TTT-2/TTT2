d12d2f46ec21cf45b64886e215dfd853797887c2
Alf21 <4lf-mueller@gmx.de>
2019-02-02 17:23:09 +0100

    check real time update#2

--- gamemodes/terrortown/gamemode/shared/hud_elements/base_elements/pure_skin_element.lua
+++ gamemodes/terrortown/gamemode/shared/hud_elements/base_elements/pure_skin_element.lua
@@ -1,9 +1,13 @@
 if CLIENT then
-	function HUDELEMENT:DrawBg(x, y, w, h, c)
+	function DrawHUDElementBg(x, y, w, h, c)
 		surface.SetDrawColor(100, 100, 100, 50)
 		surface.DrawRect(x - 1, y - 1, w + 1, h + 1)
 
 		surface.SetDrawColor(clr(c))
 		surface.DrawRect(x, y, w, h)
+	end
+
+	function HUDELEMENT:DrawBg(x, y, w, h, c)
+		DrawHUDElementBg(x, y, w, h, c)
 	end
 end
