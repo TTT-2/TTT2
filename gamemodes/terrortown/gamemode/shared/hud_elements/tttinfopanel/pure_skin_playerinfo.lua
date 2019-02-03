5509d29ff21eb7992de30bff7521832db9811744
saibotk <saibotk@users.noreply.github.com>
2019-02-03 02:53:23 +0100

    fix margin rename

--- gamemodes/terrortown/gamemode/shared/hud_elements/tttinfopanel/pure_skin_playerinfo.lua
+++ gamemodes/terrortown/gamemode/shared/hud_elements/tttinfopanel/pure_skin_playerinfo.lua
@@ -107,7 +107,7 @@ if CLIENT then
 				local sri_xoffset = w - sri_width - sri_margin_right
 
 				surface.SetDrawColor(clr(secInfoTbl.color))
-				surface.DrawRect(x + sri_xoffset, y + sri_margin, sri_width, lpw - sri_margin_top_bottom * 2)
+				surface.DrawRect(x + sri_xoffset, y + sri_margin_top_bottom, sri_width, lpw - sri_margin_top_bottom * 2)
 				self:ShadowedText(secInfoTbl.text, "PureSkinRole", x + sri_xoffset + sri_text_width_padding, y + sri_margin_top_bottom - sri_padding_inner_top_bottom, COLOR_WHITE, TEXT_ALIGN_CENTER)
 			end
 		end
