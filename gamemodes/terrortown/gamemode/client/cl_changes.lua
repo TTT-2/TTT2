fd9da2a4aa614837d17f50282670cb763797e0f4
Alf Müller <4lf-mueller@gmx.de>
2019-02-20 23:36:33 +0100

    added changelog for update 0.5.0b

--- gamemodes/terrortown/gamemode/client/cl_changes.lua
+++ gamemodes/terrortown/gamemode/client/cl_changes.lua
@@ -17,44 +17,53 @@ function CreateChanges()
 	AddChange("0.3.5.6b", [[
 	- many fixes
 	- enabled picking up grenades in prep time
-	- end scoreboard fix
+	- roundend scoreboard fix
 	- disabled useless prints that have spammed the console
 	]])
 
 	AddChange("0.3.5.7b", [[
 	- code optimization
-	- removed PLAYER:UpdateRole() function and hook "TTT2RoleTypeSet"
-	- fixed loadout and model issue on changing the role
+
 	- added different CVars
+
+	- fixed loadout and model issue on changing the role
 	- bugfixes
+
+	- removed PLAYER:UpdateRole() function and hook "TTT2RoleTypeSet"
 	]])
 
 	AddChange("0.3.5.8b", [[
-	- added different ConVars
-	- bugfixes
 	- selection system update
+
+	- added different ConVars
 	- added possibility to force a role in the next roleselection
+
+	- bugfixes
 	]])
 
 	AddChange("0.3.6b", [[
-	- karma system improvement
 	- new networking system
+
+	- added gs_crazyphysics detector
+	- added RoleVote support
+	- added new multilayed icons (+ multilayer system)
 	- added credits
 	- added changes window
 	- added detection for registered incompatible add-ons
 	- added autoslay support
+
+	- karma system improvement
 	- huge amount of bugfixes
-	- added gs_crazyphysics detector
-	- added RoleVote support
-	- added new multilayed icons (+ multilayer system)
 	]])
 
 	AddChange("0.3.7b", [[
-	- fixed TTT spec label issue
-	- removed Player:SetSubRole() and Player:SetBaseRole()
 	- added hook TTT2ToggleRole
-	- renamed hook "TTT_UseCustomPlayerModels" into "TTTUseCustomPlayerModels"
 	- added GetActiveRoles()
+
+	- fixed TTT spec label issue
+
+	- renamed hook "TTT_UseCustomPlayerModels" into "TTTUseCustomPlayerModels"
+	- removed Player:SetSubRole() and Player:SetBaseRole()
 	]])
 
 	AddChange("0.3.7.1b", [[
@@ -65,8 +74,10 @@ function CreateChanges()
 	- added possibility to set the cost (credits) for any equipment
 	- added Player:RemoveEquipmentItem(id) and Player:RemoveItem(id) / Player:RemoveBought(id)
 	- added possibility to change velocity with hook "TTT2ModifyRagdollVelocity"
+
 	- improved external icon addon support
 	- improved binding system
+
 	- fixed loadout bug
 	- fixed loadout item reset bug
 	- fixed respawn loadout issue
@@ -83,12 +94,14 @@ function CreateChanges()
 	AddChange("0.3.7.2b", [[
 	- reworked the credit system (Thanks to Nick!)
 	- added possibility to override the init.lua and cl_init.lua file in TTT2
+
 	- fixed server errors in combination with TTT Totem (now, TTT2 will just not work)
 	- fixed ragdoll collision server crash (issue is still in the normal TTT)
 	]])
 
 	AddChange("0.3.7.3b", [[
 	- reworked the selection system (balancing and bugfixes)
+
 	- fixed playermodel issue
 	- fixed toggling role issue
 	- fixed loadout doubling issue
@@ -105,6 +118,7 @@ function CreateChanges()
 	AddChange("0.3.8b", [[
 	- added new TTT2 Logo
 	- added ShopEditor missing icons (by Mineotopia)
+
 	- reworked weaponshop -> ShopEditor
 	- changed file based shopsystem into sql
 	]])
@@ -114,6 +128,7 @@ function CreateChanges()
 	- fixed item credits and minPlayers issue
 	- fixed radar, disguiser and armor issue in ItemEditor
 	- fixed shop syncing issue with ShopEditor
+
 	- small performance improvements
 	- cleaned up some useless functions
 	- removed ALL_WEAPONS table (SWEP will cache the initialized data now in it's own table / entity data)
@@ -123,6 +138,7 @@ function CreateChanges()
 	AddChange("0.3.8.2b", [[
 	- added russian translation (by Satton2)
 	- added minPlayers indicator for the shop
+
 	- replaced globalLimited param with limited param to toggle whether an item is just one time per round buyable for each player
 	]])
 
@@ -133,26 +149,53 @@ function CreateChanges()
 
 	AddChange("0.3.9b", [[
 	- reimplemented all these nice unaccepted PullRequest for TTT on GitHub:
-		- Custom Crosshairs [F1 -> Settings]: 'https://github.com/Facepunch/garrysmod/pull/1376/' by nubpro
-		- Performance improvements with the help of 'https://github.com/Facepunch/garrysmod/pull/1287' by markusmarkusz and Alf21
+	-> Custom Crosshairs [F1 -> Settings]: 'https://github.com/Facepunch/garrysmod/pull/1376/' by nubpro
+	-> Performance improvements with the help of 'https://github.com/Facepunch/garrysmod/pull/1287' by markusmarkusz and Alf21
+
 	- added possibility to use random shops! (ShopEditor -> Options)
 	- balanced karma system (Roles that are aware of their teammates can't earn karma anymore. This will lower teamkilling)
 	]])
 
 	AddChange("0.4.0b", [[
 	- ShopEditor:
-		- added .notBuyable param to hide item in the shops
-		- added .globalLimited and teamLimited params to limit equipment
+	-> added .notBuyable param to hide item in the shops
+	-> added .globalLimited and teamLimited params to limit equipment
+
 	- added new shop (by tkindanight)
-		- you can toggle the shop everytime (your role doesn't matter)
-		- the shop will update in real time
+	-> you can toggle the shop everytime (your role doesn't matter)
+	-> the shop will update in real time
+
 	- added new icons
+	- added warming up for randomness to make things happen... yea... random
+	- added auto-converter for old items
+
 	- decreased speed modification lags
 	- improved performance
-	- added warming up for randomness to make things happen... yea... random
 	- implemented new item system! TTT2 now supports more than 16 passive items!
-	- added auto-converter for old items
 	- huge amount of fixes
+	]])
+
+	AddChange("0.5.0", [[
+	- added HUDSwitcher
+	-> added old TTT HUD
+	-> added PureSkin HUD
+
+	- added possibility to give every player his own random shop
+	- added sprint to TTT2
+	- added a drowning indicator into TTT2
+	- added possibility to toggle auto afk
+	- added possibility to modify the time a player can dive
+	- added Blue's BShadow library
+	- added parameter to the bindings to detect if a player released a key
+
+	- fixed TTT2 binding system
+	- fixed Ammo pickup
+	- fixed karma issue with detective
+	- fixed DNA Scanner bug with the radar
+	- fixed loading issue in the items module
+	- fixed critical bug in the process of saving the items data
+
+	- reworked sql and created a sql library
 	]])
 
 	hook.Run("TTT2AddChange")
