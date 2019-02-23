f42f81b0d3b76696d1d390d28a0f13f8e058a51c
Alf Müller <4lf-mueller@gmx.de>
2019-02-24 00:42:08 +0100

    add BaseClass to old_ttt hud

--- gamemodes/terrortown/gamemode/shared/huds/old_ttt/cl_init.lua
+++ gamemodes/terrortown/gamemode/shared/huds/old_ttt/cl_init.lua
@@ -5,6 +5,12 @@ surface.CreateFont("TraitorState", {font = "Trebuchet24", size = 28, weight = 10
 surface.CreateFont("TimeLeft", {font = "Trebuchet24", size = 24, weight = 800})
 surface.CreateFont("HealthAmmo", {font = "Trebuchet24", size = 24, weight = 750})
 
+local base = "hud_base"
+
+DEFINE_BASECLASS(base)
+
+HUD.Base = base
+
 HUD.previewImage = Material("vgui/ttt/huds/old_ttt/preview.png")
 
 function DrawOldRoleIcon(x, y, w, h, icon, color)
