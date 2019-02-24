4dc4ad8a9041f45da493f4cd9e236a8fbb010837
Leon Masopust <leon.masopust@student.hpi.de>
2019-02-24 11:48:18 +0100

    fixed shadows with half pixel offset

--- gamemodes/terrortown/gamemode/shared/huds/pure_skin/cl_drawing_functions.lua
+++ gamemodes/terrortown/gamemode/shared/huds/pure_skin/cl_drawing_functions.lua
@@ -1,5 +1,4 @@
-local shadow_border = Material("vgui/ttt/dynamic/hud_components/shadow_border.png" )
-shadow_border = surface.GetTextureID("vgui/ttt/dynamic/hud_components/shadow_border")
+local shadow_border = surface.GetTextureID("vgui/ttt/dynamic/hud_components/shadow_border")
 
 -- x, y, width, height, color
 function DrawHUDElementBg(x, y, w, h, c)	
@@ -14,23 +13,22 @@ function DrawHUDElementLines(x, y, w, h)
 	local edge_size = 3
 
 	surface.SetDrawColor( 255, 255, 255, 255 )
-	--surface.SetMaterial( shadow_border )
 	surface.SetTexture( shadow_border )
 	
-	surface.DrawTexturedRectUV( x - shadow_size, y + h - edge_size, corner_size, corner_size, 3/63, 52/63,  10/63, 59/63 )
+	surface.DrawTexturedRectUV( x - shadow_size, y + h - edge_size, corner_size, corner_size, 3.5/63, 52.5/63,  10.5/63, 59.5/63 )
 	
-	surface.DrawTexturedRectUV( x + w - edge_size, y + h - edge_size, corner_size, corner_size, 52/63, 52/63,  59/63, 59/63 )
+	surface.DrawTexturedRectUV( x + w - edge_size, y + h - edge_size, corner_size, corner_size, 52.5/63, 52.5/63,  59.5/63, 59.5/63 )
 	
-	surface.DrawTexturedRectUV( x - shadow_size, y - shadow_size, corner_size, corner_size, 3/63, 3/63,  10/63, 10/63)
+	surface.DrawTexturedRectUV( x - shadow_size, y - shadow_size, corner_size, corner_size, 3.5/63, 3.5/63,  10.5/63, 10.5/63 )
 	
-	surface.DrawTexturedRectUV( x  + w - edge_size, y - shadow_size, corner_size, corner_size, 52/63, 3/63,  59/63, 10/63 )
+	surface.DrawTexturedRectUV( x  + w - edge_size, y - shadow_size, corner_size, corner_size, 52.5/63, 3.5/63,  59.5/63, 10.4/63 )
 	
-	surface.DrawTexturedRectUV( x + edge_size, y + h - edge_size, w - 2 * edge_size, corner_size, 32/63, 52/63,  33/63, 59/63 )
+	surface.DrawTexturedRectUV( x + edge_size, y + h - edge_size, w - 2 * edge_size, corner_size, 31.5/63, 52.5/63,  32.5/63, 59.5/63 )
 	
-	surface.DrawTexturedRectUV( x - shadow_size, y + edge_size, corner_size, h - 2 * edge_size, 3/63, 32/63,  10/63, 33/63 )
+	surface.DrawTexturedRectUV( x - shadow_size, y + edge_size, corner_size, h - 2 * edge_size, 3.5/63, 31.5/63,  10.5/63, 32.5/63 )
 	
-	surface.DrawTexturedRectUV( x + w - edge_size, y + edge_size, corner_size, h - 2 * edge_size, 52/63, 32/63,  59/63, 33/63 )
+	surface.DrawTexturedRectUV( x + w - edge_size, y + edge_size, corner_size, h - 2 * edge_size, 52.5/63, 31.5/63,  59.5/63, 32.5/63 )
 	
-	surface.DrawTexturedRectUV( x + edge_size, y - shadow_size, w - 2 * edge_size, corner_size, 32/63, 3/63,  33/63, 10/63 )
+	surface.DrawTexturedRectUV( x + edge_size, y - shadow_size, w - 2 * edge_size, corner_size, 31.5/63, 3.5/63,  32.5/63, 10.5/63 )
 	
 end
