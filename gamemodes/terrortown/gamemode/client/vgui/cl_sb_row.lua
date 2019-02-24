706fd032560f5151d72ad2c994c3fc383067ce90
Tim Goll <tim@timgoll.de>
2019-02-24 16:58:41 +0100

    another go at tooltips

--- gamemodes/terrortown/gamemode/client/vgui/cl_sb_row.lua
+++ gamemodes/terrortown/gamemode/client/vgui/cl_sb_row.lua
@@ -94,7 +94,7 @@ function PANEL:Init()
 	self.dev:SetSize(iconSizes, iconSizes)
 	self.dev:SetMouseInputEnabled(false)
 	self.dev:SetKeepAspect(true)
-	Label("TTT2 Creator", self.dev)
+	self.dev:SetTooltip("TTT2 Creator")
 
 	self.vip = vgui.Create("DImage", self)
 	self.vip:SetSize(iconSizes, iconSizes)
