61a3f37a87f9912e0f8690947b81521b5e4dcdd1
Alf Müller <4lf-mueller@gmx.de>
2019-02-26 12:18:41 +0100

    cleaned up F1

--- gamemodes/terrortown/gamemode/client/cl_help.lua
+++ gamemodes/terrortown/gamemode/client/cl_help.lua
@@ -52,15 +52,11 @@ function HELPSCRN:Show()
 	dtabs:SetPos(margin, margin * 2)
 	dtabs:SetSize(w - margin * 2, h - margin * 3 - bh)
 
+	-- now fill with content
+
 	local padding = (dtabs:GetPadding()) * 2
 
-	local tutparent = vgui.Create("DPanel", dtabs)
-	tutparent:SetPaintBackground(false)
-	tutparent:StretchToParent(margin, 0, 0, 0)
-
-	self:CreateTutorial(tutparent)
-
-	dtabs:AddSheet(GetTranslation("help_tut"), tutparent, "icon16/book_open.png", false, false, GetTranslation("help_tut_tip"))
+	-- TTT Settings
 
 	local dsettings = vgui.Create("DPanelList", dtabs)
 	dsettings:StretchToParent(0, 0, padding, 0)
@@ -68,154 +64,38 @@ function HELPSCRN:Show()
 	dsettings:SetPadding(10)
 	dsettings:SetSpacing(10)
 
-	--- Interface area
+	dtabs:AddSheet(GetTranslation("help_settings"), dsettings, "icon16/wrench.png", false, false, GetTranslation("help_settings_tip"))
 
+	--- interface settings
 	local dgui = vgui.Create("DForm", dsettings)
-	dgui:SetName(GetTranslation("set_title_gui"))
-	dgui:CheckBox(GetTranslation("set_tips"), "ttt_tips_enable")
 
-	local cb = dgui:NumSlider(GetTranslation("set_startpopup"), "ttt_startpopup_duration", 0, 60, 0)
-	if cb.Label then
-		cb.Label:SetWrap(true)
-	end
-
-	cb:SetTooltip(GetTranslation("set_startpopup_tip"))
-
-	dgui:CheckBox(GetTranslation("set_healthlabel"), "ttt_health_label")
-
-	cb = dgui:CheckBox(GetTranslation("set_fastsw"), "ttt_weaponswitcher_fast")
-	cb:SetTooltip(GetTranslation("set_fastsw_tip"))
-
-	cb = dgui:CheckBox(GetTranslation("set_fastsw_menu"), "ttt_weaponswitcher_displayfast")
-	cb:SetTooltip(GetTranslation("set_fastswmenu_tip"))
-
-	cb = dgui:CheckBox(GetTranslation("set_wswitch"), "ttt_weaponswitcher_stay")
-	cb:SetTooltip(GetTranslation("set_wswitch_tip"))
-
-	cb = dgui:CheckBox(GetTranslation("set_cues"), "ttt_cl_soundcues")
+	self:CreateInterfaceSettings(dgui)
 
 	dsettings:AddItem(dgui)
 
-	--- Gameplay area
-
+	--- gameplay settings
 	local dplay = vgui.Create("DForm", dsettings)
-	dplay:SetName(GetTranslation("set_title_play"))
 
-	for _, v in pairs(GetRoles()) do
-		if ConVarExists("ttt_avoid_" .. v.name) then
-			local rolename = GetTranslation(v.name)
-
-			cb = dplay:CheckBox(GetPTranslation("set_avoid", rolename), "ttt_avoid_" .. v.name)
-			cb:SetTooltip(GetPTranslation("set_avoid_tip", rolename))
-		end
-	end
-
-	cb = dplay:CheckBox(GetTranslation("set_specmode"), "ttt_spectator_mode")
-	cb:SetTooltip(GetTranslation("set_specmode_tip"))
-
-	-- TODO what is the following reason?
-	-- For some reason this one defaulted to on, unlike other checkboxes, so
-	-- force it to the actual value of the cvar (which defaults to off)
-	local mute = dplay:CheckBox(GetTranslation("set_mute"), "ttt_mute_team_check")
-	mute:SetValue(GetConVar("ttt_mute_team_check"):GetBool())
-	mute:SetTooltip(GetTranslation("set_mute_tip"))
+	self:CreateGameplaySettings(dsettings)
 
 	dsettings:AddItem(dplay)
 
-	-- Crosshair area
-
+	-- crosshair settings
 	local dcrosshair = vgui.Create("DForm", dsettings)
-	dcrosshair:SetName(GetTranslation("set_title_cross"))
 
-	dcrosshair:CheckBox(GetTranslation("set_cross_color_enable"), "ttt_crosshair_color_enable")
-
-	cm = vgui.Create("DColorMixer")
-	cm:SetLabel(GetTranslation("set_cross_color"))
-	cm:SetTall(120)
-	cm:SetAlphaBar(false)
-	cm:SetPalette(false)
-	cm:SetColor(Color(30, 160, 160, 255))
-	cm:SetConVarR("ttt_crosshair_color_r")
-	cm:SetConVarG("ttt_crosshair_color_g")
-	cm:SetConVarB("ttt_crosshair_color_b")
-
-	dcrosshair:AddItem(cm)
-
-	dcrosshair:CheckBox(GetTranslation("set_cross_gap_enable"), "ttt_crosshair_gap_enable")
-
-	cb = dcrosshair:NumSlider(GetTranslation("set_cross_gap"), "ttt_crosshair_gap", 0, 30, 0)
-	if cb.Label then
-		cb.Label:SetWrap(true)
-	end
-
-	cb = dcrosshair:NumSlider(GetTranslation("set_cross_opacity"), "ttt_crosshair_opacity", 0, 1, 1)
-	if cb.Label then
-		cb.Label:SetWrap(true)
-	end
-
-	cb = dcrosshair:NumSlider(GetTranslation("set_ironsight_cross_opacity"), "ttt_ironsights_crosshair_opacity", 0, 1, 1)
-	if cb.Label then
-		cb.Label:SetWrap(true)
-	end
-
-	cb = dcrosshair:NumSlider(GetTranslation("set_cross_brightness"), "ttt_crosshair_brightness", 0, 1, 1)
-	if cb.Label then
-		cb.Label:SetWrap(true)
-	end
-
-	cb = dcrosshair:NumSlider(GetTranslation("set_cross_size"), "ttt_crosshair_size", 0.1, 3, 1)
-	if cb.Label then
-		cb.Label:SetWrap(true)
-	end
-
-	cb = dcrosshair:NumSlider(GetTranslation("set_cross_thickness"), "ttt_crosshair_thickness", 1, 10, 0)
-	if cb.Label then
-		cb.Label:SetWrap(true)
-	end
-
-	cb = dcrosshair:NumSlider(GetTranslation("set_cross_outlinethickness"), "ttt_crosshair_outlinethickness", 0, 5, 0)
-	if cb.Label then
-		cb.Label:SetWrap(true)
-	end
-
-	dcrosshair:CheckBox(GetTranslation("set_cross_disable"), "ttt_disable_crosshair")
-	dcrosshair:CheckBox(GetTranslation("set_minimal_id"), "ttt_minimal_targetid")
-	dcrosshair:CheckBox(GetTranslation("set_cross_static_enable"), "ttt_crosshair_static")
-	dcrosshair:CheckBox(GetTranslation("set_cross_dot_enable"), "ttt_crosshair_dot")
-	dcrosshair:CheckBox(GetTranslation("set_cross_weaponscale_enable"), "ttt_crosshair_weaponscale")
-
-	cb = dcrosshair:CheckBox(GetTranslation("set_lowsights"), "ttt_ironsights_lowered")
-	cb:SetTooltip(GetTranslation("set_lowsights_tip"))
+	self:CreateCrosshairSettings(dcrosshair)
 
 	dsettings:AddItem(dcrosshair)
 
 	--- Language area
 	local dlanguage = vgui.Create("DForm", dsettings)
-	dlanguage:SetName(GetTranslation("set_title_lang"))
 
-	local dlang = vgui.Create("DComboBox", dlanguage)
-	dlang:SetConVar("ttt_language")
-	dlang:AddChoice("Server default", "auto")
-
-	for _, lang in pairs(LANG.GetLanguages()) do
-		dlang:AddChoice(string.Capitalize(lang), lang)
-	end
-
-	-- Why is DComboBox not updating the cvar by default?
-	dlang.OnSelect = function(idx, val, data)
-		RunConsoleCommand("ttt_language", data)
-	end
-
-	dlang.Think = dlang.ConVarStringThink
-
-	dlanguage:Help(GetTranslation("set_lang"))
-	dlanguage:AddItem(dlang)
+	self:CreateLanguageForm(dlanguage)
 
 	dsettings:AddItem(dlanguage)
 
-	dtabs:AddSheet(GetTranslation("help_settings"), dsettings, "icon16/wrench.png", false, false, GetTranslation("help_settings_tip"))
+	-- TTT2 Settings
 
-	-- role description, bindings and co
 	local ttt2_panel = vgui.Create("DPanelList", dtabs)
 	ttt2_panel:StretchToParent(0, 0, dtabs:GetPadding() * 2, 0)
 	ttt2_panel:EnableVerticalScrollbar(true)
@@ -224,6 +104,7 @@ function HELPSCRN:Show()
 
 	dtabs:AddSheet("TTT2", ttt2_panel, "icon16/information.png", false, false, "The TTT2 settings")
 
+	-- role description
 	local roledesc_tab = vgui.Create("DForm")
 	roledesc_tab:SetSpacing(10)
 
@@ -245,7 +126,6 @@ function HELPSCRN:Show()
 	end
 
 	roledesc_tab:SizeToContents()
-	-- end role description
 
 	-- changes
 	local changesButton = vgui.Create("DButton")
@@ -269,38 +149,24 @@ function HELPSCRN:Show()
 
 	--- binding area
 	local dbindings = vgui.Create("DForm", ttt2_panel)
-	dbindings:SetName("TTT2 Bindings")
 
-	for _, binding in ipairs(bind.GetSettingsBindings()) do
-		local dPlabel = vgui.Create("DLabel")
-		dPlabel:SetText(binding.label)
-
-		local dPBinder = vgui.Create("DBinder")
-		dPBinder:SetSize(170, 30)
-
-		local curBinding = bind.Find(binding.name)
-		dPBinder:SetValue(curBinding)
-
-		function dPBinder:OnChange(num)
-			if num == 0 then
-				bind.Remove(curBinding, binding.name)
-			else
-				bind.Remove(curBinding, binding.name)
-				bind.Add(num, binding.name, true)
-
-				LocalPlayer():ChatPrint("New bound key for '" .. binding.name .. "': " .. input.GetKeyName(num))
-			end
-
-			curBinding = num
-		end
-
-		dbindings:AddItem(dPlabel, dPBinder)
-	end
+	self:CreateBindings(dbindings)
 
 	ttt2_panel:AddItem(dbindings)
 
+	-- Tutorial
+	local tutparent = vgui.Create("DPanel", dtabs)
+	tutparent:SetPaintBackground(false)
+	tutparent:StretchToParent(margin, 0, 0, 0)
+
+	self:CreateTutorial(tutparent)
+
+	dtabs:AddSheet(GetTranslation("help_tut"), tutparent, "icon16/book_open.png", false, false, GetTranslation("help_tut_tip"))
+
+	-- extern support
 	hook.Call("TTTSettingsTabs", GAMEMODE, dtabs)
 
+	--
 	dframe:MakePopup()
 	dframe:SetKeyboardInputEnabled(false)
 
@@ -331,6 +197,174 @@ local function MuteTeamCallback(cv, old, new)
 	end
 end
 cvars.AddChangeCallback("ttt_mute_team_check", MuteTeamCallback)
+
+function HELPSCRN:CreateInterfaceSettings(parent)
+	parent:SetName(GetTranslation("set_title_gui"))
+	parent:CheckBox(GetTranslation("set_tips"), "ttt_tips_enable")
+
+	local cb = parent:NumSlider(GetTranslation("set_startpopup"), "ttt_startpopup_duration", 0, 60, 0)
+	if cb.Label then
+		cb.Label:SetWrap(true)
+	end
+
+	cb:SetTooltip(GetTranslation("set_startpopup_tip"))
+
+	parent:CheckBox(GetTranslation("set_healthlabel"), "ttt_health_label")
+
+	cb = parent:CheckBox(GetTranslation("set_fastsw"), "ttt_weaponswitcher_fast")
+	cb:SetTooltip(GetTranslation("set_fastsw_tip"))
+
+	cb = parent:CheckBox(GetTranslation("set_fastsw_menu"), "ttt_weaponswitcher_displayfast")
+	cb:SetTooltip(GetTranslation("set_fastswmenu_tip"))
+
+	cb = parent:CheckBox(GetTranslation("set_wswitch"), "ttt_weaponswitcher_stay")
+	cb:SetTooltip(GetTranslation("set_wswitch_tip"))
+
+	cb = parent:CheckBox(GetTranslation("set_cues"), "ttt_cl_soundcues")
+end
+
+-- language
+function HELPSCRN:CreateLanguageForm(parent)
+	parent:SetName(GetTranslation("set_title_lang"))
+
+	local dlang = vgui.Create("DComboBox", parent)
+	dlang:SetConVar("ttt_language")
+	dlang:AddChoice("Server default", "auto")
+
+	for _, lang in pairs(LANG.GetLanguages()) do
+		dlang:AddChoice(string.Capitalize(lang), lang)
+	end
+
+	-- Why is DComboBox not updating the cvar by default?
+	dlang.OnSelect = function(idx, val, data)
+		RunConsoleCommand("ttt_language", data)
+	end
+
+	dlang.Think = dlang.ConVarStringThink
+
+	parent:Help(GetTranslation("set_lang"))
+	parent:AddItem(dlang)
+end
+
+function HELPSCRN:CreateCrosshairSettings(parent)
+	parent:SetName(GetTranslation("set_title_cross"))
+
+	parent:CheckBox(GetTranslation("set_cross_color_enable"), "ttt_crosshair_color_enable")
+
+	local cm = vgui.Create("DColorMixer")
+	cm:SetLabel(GetTranslation("set_cross_color"))
+	cm:SetTall(120)
+	cm:SetAlphaBar(false)
+	cm:SetPalette(false)
+	cm:SetColor(Color(30, 160, 160, 255))
+	cm:SetConVarR("ttt_crosshair_color_r")
+	cm:SetConVarG("ttt_crosshair_color_g")
+	cm:SetConVarB("ttt_crosshair_color_b")
+
+	parent:AddItem(cm)
+
+	parent:CheckBox(GetTranslation("set_cross_gap_enable"), "ttt_crosshair_gap_enable")
+
+	local cb = parent:NumSlider(GetTranslation("set_cross_gap"), "ttt_crosshair_gap", 0, 30, 0)
+	if cb.Label then
+		cb.Label:SetWrap(true)
+	end
+
+	cb = parent:NumSlider(GetTranslation("set_cross_opacity"), "ttt_crosshair_opacity", 0, 1, 1)
+	if cb.Label then
+		cb.Label:SetWrap(true)
+	end
+
+	cb = parent:NumSlider(GetTranslation("set_ironsight_cross_opacity"), "ttt_ironsights_crosshair_opacity", 0, 1, 1)
+	if cb.Label then
+		cb.Label:SetWrap(true)
+	end
+
+	cb = parent:NumSlider(GetTranslation("set_cross_brightness"), "ttt_crosshair_brightness", 0, 1, 1)
+	if cb.Label then
+		cb.Label:SetWrap(true)
+	end
+
+	cb = parent:NumSlider(GetTranslation("set_cross_size"), "ttt_crosshair_size", 0.1, 3, 1)
+	if cb.Label then
+		cb.Label:SetWrap(true)
+	end
+
+	cb = parent:NumSlider(GetTranslation("set_cross_thickness"), "ttt_crosshair_thickness", 1, 10, 0)
+	if cb.Label then
+		cb.Label:SetWrap(true)
+	end
+
+	cb = parent:NumSlider(GetTranslation("set_cross_outlinethickness"), "ttt_crosshair_outlinethickness", 0, 5, 0)
+	if cb.Label then
+		cb.Label:SetWrap(true)
+	end
+
+	parent:CheckBox(GetTranslation("set_cross_disable"), "ttt_disable_crosshair")
+	parent:CheckBox(GetTranslation("set_minimal_id"), "ttt_minimal_targetid")
+	parent:CheckBox(GetTranslation("set_cross_static_enable"), "ttt_crosshair_static")
+	parent:CheckBox(GetTranslation("set_cross_dot_enable"), "ttt_crosshair_dot")
+	parent:CheckBox(GetTranslation("set_cross_weaponscale_enable"), "ttt_crosshair_weaponscale")
+
+	cb = parent:CheckBox(GetTranslation("set_lowsights"), "ttt_ironsights_lowered")
+	cb:SetTooltip(GetTranslation("set_lowsights_tip"))
+end
+
+function HELPSCRN:CreateGameplaySettings(parent)
+	parent:SetName(GetTranslation("set_title_play"))
+
+	local cb
+
+	for _, v in pairs(GetRoles()) do
+		if ConVarExists("ttt_avoid_" .. v.name) then
+			local rolename = GetTranslation(v.name)
+
+			cb = parent:CheckBox(GetPTranslation("set_avoid", rolename), "ttt_avoid_" .. v.name)
+			cb:SetTooltip(GetPTranslation("set_avoid_tip", rolename))
+		end
+	end
+
+	cb = parent:CheckBox(GetTranslation("set_specmode"), "ttt_spectator_mode")
+	cb:SetTooltip(GetTranslation("set_specmode_tip"))
+
+	-- TODO what is the following reason?
+	-- For some reason this one defaulted to on, unlike other checkboxes, so
+	-- force it to the actual value of the cvar (which defaults to off)
+	local mute = parent:CheckBox(GetTranslation("set_mute"), "ttt_mute_team_check")
+	mute:SetValue(GetConVar("ttt_mute_team_check"):GetBool())
+	mute:SetTooltip(GetTranslation("set_mute_tip"))
+end
+
+-- Bindings
+function HELPSCRN:CreateBindings(parent)
+	parent:SetName("TTT2 Bindings")
+
+	for _, binding in ipairs(bind.GetSettingsBindings()) do
+		local dPlabel = vgui.Create("DLabel")
+		dPlabel:SetText(binding.label)
+
+		local dPBinder = vgui.Create("DBinder")
+		dPBinder:SetSize(170, 30)
+
+		local curBinding = bind.Find(binding.name)
+		dPBinder:SetValue(curBinding)
+
+		function dPBinder:OnChange(num)
+			if num == 0 then
+				bind.Remove(curBinding, binding.name)
+			else
+				bind.Remove(curBinding, binding.name)
+				bind.Add(num, binding.name, true)
+
+				LocalPlayer():ChatPrint("New bound key for '" .. binding.name .. "': " .. input.GetKeyName(num))
+			end
+
+			curBinding = num
+		end
+
+		parent:AddItem(dPlabel, dPBinder)
+	end
+end
 
 --- Tutorial
 
