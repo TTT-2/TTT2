c277fad6b0d4cd67a9fdae04ea90e4cb4f7d56bc
Tim Goll <tim@timgoll.de>
2019-02-26 16:31:04 +0100

    fixed bool error

--- gamemodes/terrortown/gamemode/client/cl_hud_manager.lua
+++ gamemodes/terrortown/gamemode/client/cl_hud_manager.lua
@@ -88,43 +88,46 @@ local function EditLocalHUD()
 			local alt_pressed = input.IsKeyDown(KEY_LALT) or input.IsKeyDown(KEY_LALT)
 			trans_data = elem:GetClickedArea(x, y, alt_pressed)
 
-			if trans_data.move then
-				chat.AddText("move element")
-			else
-				chat.AddText(tostring(trans_data.x_p), ", ", tostring(trans_data.x_m), ", ", tostring(trans_data.y_p), ", ", tostring(trans_data.y_m), ", ", tostring(trans_data.direction_x), ", ", tostring(trans_data.direction_y))
-			end
-
-			if trans_data.move then -- move mode
-				local nx = x - difX
-				local ny = y - difY
-
-				if nx < 0 then
-					nx = 0
-				elseif nx + size.w > ScrW() then
-					nx = ScrW() - size.w
+			if trans_data then
+				if trans_data.move then
+					chat.AddText("move element")
+				else
+					chat.AddText(tostring(trans_data.x_p), ", ", tostring(trans_data.x_m), ", ", tostring(trans_data.y_p), ", ", tostring(trans_data.y_m), ", ", tostring(trans_data.direction_x), ", ", tostring(trans_data.direction_y))
 				end
 
-				if ny < 0 then
-					ny = 0
-				elseif ny + size.h > ScrH() then
-					ny = ScrH() - size.h
+				if trans_data.move then -- move mode
+					local nx = x - difX
+					local ny = y - difY
+
+					if nx < 0 then
+						nx = 0
+					elseif nx + size.w > ScrW() then
+						nx = ScrW() - size.w
+					end
+
+					if ny < 0 then
+						ny = 0
+					elseif ny + size.h > ScrH() then
+						ny = ScrH() - size.h
+					end
+
+					elem:SetBasePos(nx + client.difBaseX, ny + client.difBaseY)
+				else -- resize mode
+					local size = elem:GetSize() -- curent size
+					local pos = elem:GetPos() -- current pos
+
+					local multi_w = (trans_data.x_p and 1 or 0) + (trans_data.x_n and 1 or 0)
+					local multi_w = (trans_data.y_p and 1 or 0) + (trans_data.y_n and 1 or 0)
+					local new_w = size.w + (client.oldMX - x) * trans_data.direction_x * multi_w
+					local new_h = size.h + (client.oldMY - y) * trans_data.direction_y * multi_y
+
+					elem:SetSize(new_w, new_h)
+					elem:SetBasePos(trans_data.x_n and pos.x + trans_data.direction_x * (client.oldMX - x) or pos.x, trans_data.y_n and pos.y + trans_data.direction_y * (client.oldMY - y) or pos.y)
 				end
 
-				elem:SetBasePos(nx + client.difBaseX, ny + client.difBaseY)
-			else -- resize mode
-				local size = elem:GetSize() -- curent size
-				local pos = elem:GetPos() -- current pos
-
-				local multi_w = (trans_data.x_p and 1 or 0) + (trans_data.x_n and 1 or 0)
-				local multi_w = (trans_data.y_p and 1 or 0) + (trans_data.y_n and 1 or 0)
-				local new_w = size.w + (client.oldMX - x) * trans_data.direction_x * multi_w
-				local new_h = size.h + (client.oldMY - y) * trans_data.direction_y * multi_y
-
-				elem:SetSize(new_w, new_h)
-				elem:SetBasePos(trans_data.x_n and pos.x + trans_data.direction_x * (client.oldMX - x) or pos.x, trans_data.y_n and pos.y + trans_data.direction_y * (client.oldMY - y) or pos.y)
+				elem:PerformLayout()
 			end
 
-			elem:PerformLayout()
 		end
 	else
 		elem = nil
