f70473159bab849e2877cadd0c5ca253e15462c7
Leon Masopust <leon.masopust@student.hpi.de>
2019-03-09 21:07:36 +0100

    added scalable_hud as abstract base for pure_skin

--- gamemodes/terrortown/gamemode/shared/huds/pure_skin/cl_init.lua
+++ gamemodes/terrortown/gamemode/shared/huds/pure_skin/cl_init.lua
@@ -11,45 +11,13 @@ surface.CreateFont("PureSkinWepNum", {font = "Trebuchet24", size = 21, weight = 
 -- base drawing functions
 include("cl_drawing_functions.lua")
 
-local base = "hud_base"
+local base = "scalable_hud"
 
 DEFINE_BASECLASS(base)
 
 HUD.Base = base
 
-local defaultColor = Color(49, 71, 94)
-local defaultScale = 1.0
-
 HUD.previewImage = Material("vgui/ttt/huds/pure_skin/preview.png")
-
-local savingKeys
-
-function HUD:GetSavingKeys()
-	if not savingKeys then
-		savingKeys = BaseClass.GetSavingKeys(self)
-		savingKeys.basecolor = {
-			typ = "color",
-			desc = "BaseColor",
-			OnChange = function(slf, col) 
-				self:PerformLayout()
-			end
-		}
-		savingKeys.scale = {
-			typ = "scale",
-			desc = "Reset Positions and set HUD Scale",
-			OnChange = function(slf, val)
-				local scaleMultiplier = val / self.scale
-				self:ApplyScale(scaleMultiplier)
-				self:SaveData()
-			end	
-		}
-	end
-
-	return table.Copy(savingKeys)
-end
-
-HUD.basecolor = defaultColor
-HUD.scale = defaultScale
 
 function HUD:Initialize()
 	self:ForceElement("pure_skin_playerinfo")
@@ -63,39 +31,6 @@ function HUD:Initialize()
 	self:ForceElement("pure_skin_target")
 
 	BaseClass.Initialize(self)
-end
-
-function HUD:Loaded()
-	for _, elem in ipairs(self:GetElements()) do
-		local el = hudelements.GetStored(elem)
-		if el then
-			local min_size = el.defaults.minsize
-			el:SetMinSize(min_size.w * self.scale, min_size.h * self.scale)
-			el:PerformLayout()
-		end
-	end
-end
-
-function HUD:ApplyScale(scale)
-	for _, elem in ipairs(self:GetElements()) do
-		local el = hudelements.GetStored(elem)
-		if el then
-			local size = el:GetSize()
-			local min_size = el:GetMinSize()
-			el:SetMinSize(min_size.w * scale, min_size.h * scale)
-			el:SetSize(size.w * scale, size.h * scale)
-			el:PerformLayout()      --code from here will be replaced by a Reset call
-			el:RecalculateBasePos()
-			el:PerformLayout()
-		end
-	end
-end
-
-function HUD:Reset()
-	self.basecolor = defaultColor
-	BaseClass.Reset(self)
-
-	self:ApplyScale(self.scale)
 end
 
 -- Voice overriding
