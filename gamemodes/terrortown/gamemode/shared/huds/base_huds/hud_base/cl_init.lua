0f4ef7818c82f3cdb681f9d2629c0683fc806b82
saibotk <saibotk@users.noreply.github.com>
2019-03-10 17:13:49 +0100

    added documentation for the HUD base element

--- gamemodes/terrortown/gamemode/shared/huds/base_huds/hud_base/cl_init.lua
+++ gamemodes/terrortown/gamemode/shared/huds/base_huds/hud_base/cl_init.lua
@@ -1,32 +1,54 @@
-----------------------------------------------
--- HUD Base class
-----------------------------------------------
-HUD.elements = {}
+------------
+-- HUD Base class.
+-- @module HUD
+
+HUD.forcedElements = {}
 HUD.disabledTypes = {}
+
+--- The preview image that is shown in the HUD-Switcher.
+-- Has to be a GMOD Material!
 HUD.previewImage = Material("vgui/ttt/score_logo_2")
 
 local savingKeys = {}
 
+--- This function will return a table containing all keys that will be stored by
+-- the @{HUD:SaveData} function.
+-- @treturn tab
 function HUD:GetSavingKeys()
 	return savingKeys
 end
 
+--- This function will add an element to the forced elements for this HUD, so
+-- this will define the implementation used for a type. Use this on knonw
+-- elements you want to be shown in your HUD, so the HUD doesn't select the
+-- first implementation it finds.
+-- @tparam string
 function HUD:ForceElement(elementID)
 	local elem = hudelements.GetStored(elementID)
 
-	if elem and elem.type and not self.elements[elem.type] then
-		self.elements[elem.type] = elementID
+	if elem and elem.type and not self.forcedElements[elem.type] then
+		self.forcedElements[elem.type] = elementID
 	end
 end
 
+--- This will give you a copy of the forced elements table.
+-- @treturn tab
 function HUD:GetForcedElements()
 	return table.Copy(self.forcedElements)
 end
 
+--- This will set a type to be hidden.
+-- The element implementation for this type then will not be drawn anymore.
+-- @tparam string
 function HUD:HideType(elementType)
 	table.insert(self.disabledTypes, elementType)
 end
 
+--- This will determine if an element type is supposed to be displayed and it
+-- will respect the parent->child relation, so if a parent is hidden, the child
+-- won't show either.
+-- @tparam string
+-- @treturn bool
 function HUD:ShouldShow(elementType)
 	local el = self:GetElementByType(elementType)
 	if el then
@@ -46,6 +68,9 @@ function HUD:ShouldShow(elementType)
 	end
 end
 
+--- This will tell all elements in this HUD to recalculate their positions/sizes
+-- and other dependent variables. This is called whenever the HUD changes or
+-- loads new values either for itself (eg. basecolor) or for its children.
 function HUD:PerformLayout()
 	for _, elemName in ipairs(self:GetElements()) do
 		local elem = hudelements.GetStored(elemName)
@@ -61,6 +86,9 @@ function HUD:PerformLayout()
 	end
 end
 
+--- This will initialize the HUD, by calling @{HUDELEMENT:Initialze} on its
+-- elements, respecting the parent -> child relations. It will also call
+-- @{HUD:PerformLayout} before setting the @{HUDELEMENT.initialized} parameter.
 function HUD:Initialize()
 	-- Initialize elements default values
 	for _, v in ipairs(self:GetElements()) do
@@ -87,6 +115,21 @@ function HUD:Initialize()
 	end
 end
 
+--- Returns wether or not the HUD has an element for the given type.
+-- See @{HUD:GetElementByType} for an explaination when a HUD 'has' an element.
+-- @tparam string
+-- @treturn bool
+function HUD:HasElementType(elementType)
+	return self:GetElementByType(elementType) ~= nil
+end
+
+--- Returns an element the HUD wants to be used for a given type.
+-- It will only return the element if the HUD 'has' the element,
+-- here 'has' means the element is not hidden by @{HUD:HideType} or for example
+-- doesn't have the @{HUDELEMENT.disabledUnlessForced} attribute set and is not
+-- explicitly forced by @{HUD:ForceElement}.
+-- @tparam string
+-- @treturn tab
 function HUD:GetElementByType(elementType)
 	if table.HasValue(self.disabledTypes, elementType) then
 		return nil
@@ -123,10 +166,10 @@ function HUD:GetElementByType(elementType)
 	end
 end
 
-function HUD:HasElementType(elementType)
-	return self:GetElementByType(elementType) ~= nil
-end
-
+--- This returns a table with all the specific elements the HUD has. One element
+-- per type, respecting the forcedElements and otherwise taking the first found
+-- implementation.
+-- @treturn tab
 function HUD:GetElements()
 	-- loop through all types and if the hud does not provide an element take the first found instance for the type
 	local elems = {}
@@ -140,6 +183,11 @@ function HUD:GetElements()
 	return elems
 end
 
+--- Called to draw all elements, by calling the @{HUDELEMENT:Draw} function on
+-- them. This will also respect the @{HUDELEMENT.initialized} attribute, the
+-- @{HUD:ShouldShow} result and the result of the hook "HUDShouldDraw".
+-- This will also respect the parent -> child relations and will only call
+-- @{HUDELEMENT:Draw} on non-child elements.
 function HUD:Draw()
 	for _, elemName in ipairs(self:GetElements()) do
 		local elem = hudelements.GetStored(elemName)
@@ -160,6 +208,9 @@ function HUD:Draw()
 	end
 end
 
+--- This will reset all elements of the HUD and call @{HUDELEMENT:Reset} on its
+-- elements. This will respect the parent -> child relation and only call this
+-- on non-child elements.
 function HUD:Reset()
 	for _, elemName in ipairs(self:GetElements()) do
 		local elem = hudelements.GetStored(elemName)
@@ -175,6 +226,9 @@ function HUD:Reset()
 	end
 end
 
+--- This will save all data of the HUD and its elements, by calling
+-- {@HUDELEMENT:SaveData} on them. This is usually called when changing the
+-- current HUD or editing a HUD in the HUDEditor.
 function HUD:SaveData()
 	-- save data for the HUD
 	SQL.Save("ttt2_huds", self.id, self, self:GetSavingKeys())
@@ -188,6 +242,10 @@ function HUD:SaveData()
 	end
 end
 
+--- This function will load all saved keys from the Database and will then call
+-- @{HUDELEMENT:LoadData} on all elements, shown by the HUD. After that it will
+-- call @{HUD:PerformLayout}. This function is called when the HUDManager loads
+-- / changes to this HUD.
 function HUD:LoadData()
 	local skeys = self:GetSavingKeys()
 
