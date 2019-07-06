44d2700fefe29ada64201c1a47a46e63bd075aca
Leon Masopust <leon.masopust@student.hpi.de>
2019-07-06 18:16:30 +0200

    huge refactorings and bug fixes (scale was loaded as string until now...)

--- gamemodes/terrortown/gamemode/client/cl_fonts.lua
+++ gamemodes/terrortown/gamemode/client/cl_fonts.lua
@@ -1,42 +1,39 @@
 FONTS = {}
+FONTS.fonts = {}
+FONTS.Scales = {1, 1.5, 2, 2.5}
 
 local shadowColorDark = Color(0, 0, 0, 200)
 local shadowColorWhite = Color(200, 200, 200, 200)
 
-local function getScaleModifier(scale)
-    print(scale)
-
+local function getScaleModifier(scale)      
     local scaleFactor = isvector(scale) and math.max(scale.x, math.max(scale.y, scale.z)) or scale
-    if scaleFactor < 0.75 then
-        return 0.5
-    elseif scaleFactor < 1.5 then
-        return 1
-    else
-        return 2
+    scaleFactor = tonumber(scaleFactor)
+    
+    for _, scale in ipairs(FONTS.Scales) do
+        if scaleFactor <= scale then
+            return scale
+        end
     end
+    
+    --fallback (return the last scale)
+    return FONTS.Scales[#FONTS.Scales]
 end
 
 function draw.CreateAdvancedFont(fontName, fontData)
-    local fontName2 = fontName .. "2" 
-    local fontName05 = fontName .. "0.5" 
+    local originalSize = fontData.size
     
-    FONTS[fontName] = {}
+    FONTS.fonts[fontName] = {}
 
-    FONTS[fontName][0.5] = fontName05
-    FONTS[fontName][1] = fontName
-    FONTS[fontName][2] = fontName2
+    for _, scale in ipairs(FONTS.Scales) do
+        --note: the original font should be always created
+        local scaledFontName = scale == 1 and fontName or fontName .. tostring(scale)
 
-    --create original font
-    surface.CreateFont(fontName, fontData)
+        --create font
+        fontData.size = scale * originalSize
+        surface.CreateFont(scaledFontName, fontData)
 
-    --create font with halfed size
-    fontData.size = 0.5 * fontData.size
-    surface.CreateFont(fontName05, fontData)
-    fontData.size = 2 * fontData.size
-
-    --create font with doubled size
-    fontData.size = 2 * fontData.size
-    surface.CreateFont(fontName2, fontData)
+        FONTS.fonts[fontName][scale] = scaledFontName
+    end
 end
 
 function draw.ShadowedText(text, font, x, y, color, xalign, yalign, scaleModifier)
@@ -49,9 +46,9 @@ end
 
 function draw.AdvancedText(text, font, x, y, color, xalign, yalign, shadow, scale)
     local scaleModifier = 1.0
-    if FONTS[font] then
+    if FONTS.fonts[font] then
         scaleModifier = getScaleModifier(scale)
-        font = FONTS[font][scaleModifier]
+        font = FONTS.fonts[font][scaleModifier]
         scale = scale / scaleModifier
     end
     
@@ -62,8 +59,8 @@ function draw.AdvancedText(text, font, x, y, color, xalign, yalign, shadow, scal
         mat:Scale(isvector(scale) and scale or Vector(scale, scale, scale))
         mat:Translate(-Vector(ScrW() * 0.5, ScrH() * 0.5))
 
-        render.PushFilterMag(TEXFILTER.ANISOTROPIC)
-        render.PushFilterMin(TEXFILTER.ANISOTROPIC)
+        render.PushFilterMag(TEXFILTER.LINEAR)
+        render.PushFilterMin(TEXFILTER.LINEAR)
 
         cam.PushModelMatrix(mat)
 
