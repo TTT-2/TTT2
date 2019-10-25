1fea3216297e0379dcca68132514437ede259e31
Alf Müller <4lf-mueller@gmx.de>
2019-10-25 23:31:00 +0200

    Added possibility to switch a weapon with a slot weapon if not selected

--- gamemodes/terrortown/gamemode/server/sv_weapon_pickup.lua
+++ gamemodes/terrortown/gamemode/server/sv_weapon_pickup.lua
@@ -4,20 +4,26 @@ net.Receive("ttt2_switch_weapon", function(_, ply)
 	-- player and wepaon must be valid
 	if not IsValid(ply) or not ply:IsTerror() or not ply:Alive() then return end
 
-	local activeWeapon = ply:GetActiveWeapon()
-
-	if not IsValid(activeWeapon) or not activeWeapon.AllowDrop then return end
-
 	-- handle weapon switch
 	local tracedWeapon = ply:GetEyeTrace().Entity
 
-	if not IsValid(tracedWeapon) or not tracedWeapon:IsWeapon()
-	or ply:GetPos():Distance(tracedWeapon:GetPos()) > 100
-	or tracedWeapon.Kind ~= activeWeapon.Kind then
+	if not IsValid(tracedWeapon) or not tracedWeapon:IsWeapon() then return end
+
+	local throwWeapon = ply:GetActiveWeapon()
+
+	if not IsValid(throwWeapon) or not throwWeapon.AllowDrop or throwWeapon.Kind ~= tracedWeapon.Kind then
+		local weps = ply.inventory[MakeKindValid(tracedWeapon.Kind)]
+		if not weps or #weps == 0 then return end
+
+		throwWeapon = weps[1]
+	end
+
+	if not IsValid(throwWeapon) or not throwWeapon.AllowDrop
+ 	or ply:GetPos():Distance(tracedWeapon:GetPos()) > 100 then
 		return
 	end
 
-	ply:DropWeapon(activeWeapon)
+	ply:DropWeapon(throwWeapon)
 
 	local wepCls = tracedWeapon:GetClass()
 	local clip1 = tracedWeapon:Clip1()
