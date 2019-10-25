b86c0571debd91d6b90c377208838b502e09d464
Alf Müller <4lf-mueller@gmx.de>
2019-10-25 20:26:34 +0200

    removed these inconsistent spaces

--- gamemodes/terrortown/gamemode/shared/hud_elements/base_elements/base_stacking_element.lua
+++ gamemodes/terrortown/gamemode/shared/hud_elements/base_elements/base_stacking_element.lua
@@ -19,33 +19,33 @@ HUDELEMENT.lastCount = 0
 function HUDELEMENT:Draw()
 	local elems = self.elements
 
-  --set size beforehand if #elements was reduced to remove flickering
-  if #elems < self.lastCount then
-    local height = 0
+	-- set size beforehand if #elements was reduced to remove flickering
+	if #elems < self.lastCount then
+		local height = 0
 
-    for i = 1, #elems do
-      height = height + elems[i].h + self.element_margin
-    end
+		for i = 1, #elems do
+			height = height + elems[i].h + self.element_margin
+		end
 
-    self:SetSize(self.size.w, -height)
-  end
+		self:SetSize(self.size.w, -height)
+	end
 
-  --draw all elements
-  local running_y = self.pos.y
+	-- draw all elements
+	local running_y = self.pos.y
 
-  for i = 1, #elems do
+	for i = 1, #elems do
 		local el = elems[i]
 
-    self:DrawElement(i, self.pos.x, running_y, self.size.w, el.h)
+		self:DrawElement(i, self.pos.x, running_y, self.size.w, el.h)
 
-    running_y = running_y + el.h + self.element_margin
-  end
+		running_y = running_y + el.h + self.element_margin
+	end
 
-  local totalHeight = running_y - self.pos.y
+	local totalHeight = running_y - self.pos.y
 
-  self.lastCount = #elems
+	self.lastCount = #elems
 
-  self:SetSize(self.size.w, - math.max(totalHeight, self.minsize.h))
+	self:SetSize(self.size.w, - math.max(totalHeight, self.minsize.h))
 end
 
 ---
@@ -66,7 +66,7 @@ end
 -- @param table elements list of @{HUDELEMENT}
 -- @realm client
 function HUDELEMENT:SetElements(elements)
-  self.elements = elements
+	self.elements = elements
 end
 
 ---
@@ -74,5 +74,5 @@ end
 -- @param number margin
 -- @realm client
 function HUDELEMENT:SetElementMargin(margin)
-  self.element_margin = margin
+	self.element_margin = margin
 end
