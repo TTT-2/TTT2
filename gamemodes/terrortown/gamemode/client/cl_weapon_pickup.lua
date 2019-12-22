40d46129bca2ca935f36136dd882f21c7a328991
Tim Goll <tim@timgoll.de>
2019-12-22 18:06:38 +0100

    added PlayerReady check to wCache refresh

--- gamemodes/terrortown/gamemode/client/cl_weapon_pickup.lua
+++ gamemodes/terrortown/gamemode/client/cl_weapon_pickup.lua
@@ -29,6 +29,10 @@ end
 net.Receive("ttt2_switch_weapon_update_cache", function()
 	-- this for now is a workaround to test if the timing of the refresh is the problem
 	timer.Simple(0.25, function()
+		local client = LocalPlayer()
+
+		if not IsValid(client) or not client:IsReady() then return end
+
 		WSWITCH:UpdateWeaponCache()
 	end)
 end)
