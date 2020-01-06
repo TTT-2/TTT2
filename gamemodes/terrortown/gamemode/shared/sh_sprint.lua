c149af92480b4d83947754212686bf32c7e17cd4
TheNickSkater <beinik6@gmx.at>
2020-01-06 19:02:30 +0100

    Translation fix

--- gamemodes/terrortown/gamemode/shared/sh_sprint.lua
+++ gamemodes/terrortown/gamemode/shared/sh_sprint.lua
@@ -47,8 +47,8 @@ if SERVER then
 	end)
 else
 	-- The helptext can't be changed once the convar was created, so we go with english, since it is probably the most common lang
-	local doubletap_sprint_anykey = CreateClientConVar("ttt2_doubletap_sprint_anykey", 0, true, false, LANG.GetTranslationFromLanguage("disable_doubletap_sprint", "english"), 0, 1)
-	local disable_doubletap_sprint = CreateClientConVar("ttt2_disable_doubletap_sprint", "0", true, false, LANG.GetTranslationFromLanguage("doubletap_sprint_anykey", "english"), 0, 1)
+	local doubletap_sprint_anykey = CreateClientConVar("ttt2_doubletap_sprint_anykey", 0, true, false, LANG.GetTranslationFromLanguage("doubletap_sprint_anykey", "english"), 0, 1)
+	local disable_doubletap_sprint = CreateClientConVar("ttt2_disable_doubletap_sprint", "0", true, false, LANG.GetTranslationFromLanguage("disable_doubletap_sprint", "english"), 0, 1)
 	local lastPress = 0
 	local lastPressedMoveKey = nil
 
