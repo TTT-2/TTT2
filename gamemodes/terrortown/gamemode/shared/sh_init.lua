3bc9e960f7cc95cbce54212fa26fde4445d83695
Tim Goll <tim@timgoll.de>
2020-05-04 13:47:06 +0200

    clened up code

--- gamemodes/terrortown/gamemode/shared/sh_init.lua
+++ gamemodes/terrortown/gamemode/shared/sh_init.lua
@@ -525,6 +525,7 @@ include("includes/modules/pon.lua")
 include("ttt2/extensions/net.lua")
 include("ttt2/extensions/string.lua")
 include("ttt2/extensions/table.lua")
+include("ttt2/libraries/door.lua")
 
 -- include ttt required files
 ttt_include("sh_util")
