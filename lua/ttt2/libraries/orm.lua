e897a8d46a4a868813dbac14e58998f8b4e39902
Histalek <16392835+Histalek@users.noreply.github.com>
2020-06-25 20:06:16 +0200

    fixes and optimizations

--- lua/ttt2/libraries/orm.lua
+++ lua/ttt2/libraries/orm.lua
@@ -13,108 +13,6 @@ orm = orm or {}
 
 local ormodel = {}
 
-local function SetupModelFunctions(tableName, primaryKey, dataStructure)
-	local model = {}
-	local sanTableName = sql.SQLIdent(tableName)
-
-	model.New = function(self, data)
-		local object = data or {}
-
-		-- TODO needed?
-		--object.BaseClass = baseclass.Get(tableName)
-
-		object.Save = self.Save --object.BaseClass.Save
-		object.Delete = self.Delete --object.BaseClass.Delete
-
-		return object
-	end
-
-	model.All = function(self)
-		return sql.Query("SELECT * FROM " .. sanTableName)
-	end
-
-	model.Find = function(self, primaryValue)
-		local where
-
-		if not istable(primaryValue) and #primaryKey == 1 then
-			where = sql.SQLIdent(primaryKey[1]) .. "=" .. sql.SQLStr(primaryValue)
-		elseif istable(primaryValue) and #primaryValue == #primaryKey then
-			for i = 1, #primaryKey do
-				if not where then
-					where = sql.SQLIdent(primaryKey[i]) .. "=" .. sql.SQLStr(primaryValue[i])
-				else
-					where = where .. " AND " .. sql.SQLIdent(primaryKey[i]) .. "=" .. sql.SQLStr(primaryValue[i])
-				end
-			end
-		else
-			print("[ORM] Number of primaryvalues does not match number of primarykeys!")
-			return false
-		end
-
-		local result = sql.QueryRow("SELECT * FROM " .. sanTableName .. " WHERE " .. where)
-
-		return self:New(result)
-	end
-
-	-- DO NOT setup delete/save functions if no primarykey is found.
-	-- In those cases the 'rowid' column would function as the primarykey, but as the rowid could change anytime (https://www.sqlite.org/rowidtable.html) data could be deleted unintentionally.
-	-- Most likely rowids wont change in gmod as there is no vacuum operation but just to be safe we will not allow to use such tables. ref: https://wiki.facepunch.com/gmod/sql
-	if not primaryKey then return model end
-
-	-- Prepare strings that will not change unless the model itself changes. So we don't have to create these strings everytime we use `model.Save()`.
-	local columnList
-	local primaryKeyList
-
-	for i = 1, #primaryKey do
-		if i == 1 then
-			primaryKeyList = primaryKey[1]
-		else
-			primaryKeyList = primaryKeyList .. primaryKey[i]
-		end
-	end
-
-	for i = 1, #dataStructure do
-		if i == 1 then
-			columnList = dataStructure[1]
-		else
-			columnList = columnList .. dataStructure[i]
-		end
-	end
-
-	model.Save = function(self)
-		local query = "INSERT INTO " .. sanTableName .. "("
-		local valueList
-
-		for i = 1, #dataStructure do
-			if i == 1 then
-				valueList = self[dataStructure[1]]
-			else
-				valueList = valueList .. self[dataStructure[i]]
-			end
-		end
-
-		query = query .. columnList .. ") VALUES(" .. valueList .. ") ON CONFLICT(" .. primaryKeyList .. ") DO UPDATE SET(" .. columnList .. ")=(" .. valueList .. ");"
-
-		return sql.Query(query)
-	end
-
-	model.Delete = function(self)
-		local where
-
-		for i = 1, #primaryKey do
-			if not where then
-				where = sql.SQLIdent(primaryKey[1]) .. "=" .. sql.SQLStr(self[primaryKey[1]])
-			else
-				where = where .. " AND " .. sql.SQLIdent(primaryKey[i]) .. "=" .. sql.SQLStr(self[primaryKey[i]])
-			end
-		end
-
-		return sql.Query("DELETE FROM " .. sanTableName .. " WHERE " .. where)
-	end
-
-	return model
-end
-
 ---
 -- Returns an object relational model according to the specified databasetable. Does nothing if no databasetable with the given name exists.
 -- @param string tableName The name of the table in the database to create a model for.
@@ -126,12 +24,40 @@ function orm.Make(tableName, force)
 
 	if not sql.TableExists(tableName) then return end
 
+	local model = {}
+	--baseclass.Set(tableName, model)
 	local primaryKey = sql.GetPrimaryKey(tableName)
 	local dataStructure = sql.GetTableColumns(tableName)
 
-	local model = SetupModelFunctions(tableName, primaryKey, dataStructure)
+	model._tableName = tableName
+	model._primaryKey = primaryKey
+	model._dataStructure = dataStructure
+	model.All = ormodel.All
+	model.Find = ormodel.Find
+	model.New = ormodel.New
 
-	--baseclass.Set(tableName, model)
+	-- DO NOT setup delete/save functions if no primarykey is found.
+	-- In those cases the 'rowid' column would function as the primarykey, but as the rowid could change anytime (https://www.sqlite.org/rowidtable.html) data could be deleted unintentionally.
+	-- Most likely rowids wont change in gmod as there is no vacuum operation but just to be safe we will not allow to use such tables. ref: https://wiki.facepunch.com/gmod/sql
+	if not primaryKey then return model end
+
+	model.Delete = ormodel.Delete
+	model.Save = ormodel.Save
+
+	-- Prepare strings that will not change unless the model itself changes. So we don't have to create these strings everytime we use `model.Save()`.
+	local columnList = {nil, nil}
+
+	for i = 1, #primaryKey do
+		columnList[i] = sql.SQLIdent(primaryKey[i])
+	end
+
+	model._primaryKeyList = table.concat(columnList, ",")
+
+	for i = 1, #dataStructure do
+		columnList[i] = sql.SQLIdent(dataStructure[i])
+	end
+
+	model._columnList = table.concat(columnList, ",")
 
 	orm[tableName] = model
 
@@ -142,19 +68,46 @@ end
 -- Retrieves all saved objects of the model from the database.
 -- @return table Returns an array of all found objects.
 function ormodel:All()
+	return sql.Query("SELECT * FROM " .. sql.SQLIdent(self._tableName))
 end
 
 ---
 -- Deletes the given object from the database storage.
 function ormodel:Delete()
+	local where = {}
+
+	for i = 1, #self._primaryKey do
+		where[i] = sql.SQLIdent(self._primaryKey[i]) .. "=" .. sql.SQLStr(self[self._primaryKey[i]])
+	end
+
+	where = table.concat(where, " AND ")
+
+	return sql.Query("DELETE FROM " .. sql.SQLIdent(self._tableName) .. " WHERE " .. where)
 end
 
 ---
 -- Retrieves a specific object by their primarykey from the database.
 -- @param int|string|table primaryValue The value(s) of the primarykey to search for.
 -- @note In the case of multiple columns in the primarykey you have to specify the corresponding values in the same order.
--- @return table Returns the table of the found object. Returns `false` if the number of supplied primaryvalues does not match the number of elements in the primarykey of that model.
+-- @return table Returns the table of the found object. Returns `false` if the number of supplied primaryvalues does not match the number of elements in the primarykey. Returns `nil` if no object is found.
 function ormodel:Find(primaryValue)
+	local where = {}
+
+	if not istable(primaryValue) and #self._primaryKey == 1 then
+		where = sql.SQLIdent(self._primaryKey[1]) .. "=" .. sql.SQLStr(primaryValue)
+	elseif istable(primaryValue) and #primaryValue == #self._primaryKey then
+		for i = 1, #self._primaryKey do
+			where[i] = sql.SQLIdent(self._primaryKey[i]) .. "=" .. sql.SQLStr(primaryValue[i])
+		end
+		where = table.concat(where, " AND ")
+	else
+		print("[ORM] Number of primaryvalues does not match number of primarykeys!")
+		return false
+	end
+
+	local result = sql.QueryRow("SELECT * FROM " .. sql.SQLIdent(self._tableName) .. " WHERE " .. where)
+
+	if result then return self:New(result) end
 end
 
 ---
@@ -162,9 +115,35 @@ end
 -- @param[opt] table data preexisting data the model should be initialized with.
 -- @return table The created object.
 function ormodel:New(data)
+	local object = data or {}
+
+	--object.BaseClass = baseclass.Get(self._tableName)
+
+	object.Save = self.Save --object.BaseClass.Save
+	object.Delete = self.Delete --object.BaseClass.Delete
+	object._tableName = self._tableName --object.BaseClass._tableName
+	object._dataStructure = self._dataStructure --object.BaseClass._dataStructure
+	object._primaryKey = self._primaryKey --object.BaseClass._primaryKey
+	object._primaryKeyList = self._primaryKeyList --object.BaseClass._primaryKeyList
+	object._columnList = self._columnList --object.BaseClass._columnList
+
+	return object
 end
 
 ---
 -- Saves the data of the given object to the database storage.
 function ormodel:Save()
+	local query = "INSERT INTO " .. sql.SQLIdent(self._tableName) .. "("
+	local valueList = {nil, nil}
+	local dataStructure = self._dataStructure
+
+	for i = 1, #dataStructure do
+		valueList[i] = sql.SQLStr(self[dataStructure[i]])
+	end
+
+	valueList = table.concat(valueList, ",")
+
+	query = query .. self._columnList .. ") VALUES(" .. valueList .. ") ON CONFLICT(" .. self._primaryKeyList .. ") DO UPDATE SET(" .. self._columnList .. ")=(" .. valueList .. ");"
+
+	return sql.Query(query)
 end
