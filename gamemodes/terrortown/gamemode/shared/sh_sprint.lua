324c6d7d47cd9386cf6d7c7d596877ddc5fe5450
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2020-07-02 20:23:39 +0200

    Sprint: airborne players can no longer replenish stamina (#605)
    * airborne players can no longer replenish stamina

--- gamemodes/terrortown/gamemode/shared/sh_sprint.lua
+++ gamemodes/terrortown/gamemode/shared/sh_sprint.lua
@@ -119,6 +119,9 @@ function UpdateSprint()
 
 	for i = 1, #plys do
 		local ply = plys[i]
+
+		if not ply:OnGround() then continue end
+
 		local wantsToMove = ply:KeyDown(IN_FORWARD) or ply:KeyDown(IN_BACK) or ply:KeyDown(IN_MOVERIGHT) or ply:KeyDown(IN_MOVELEFT)
 
 		if ply.sprintProgress == 1 and (not ply.isSprinting or not wantsToMove) then continue end
