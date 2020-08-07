76351ce63d80edf936ecb0d501da5ee806aae27e
Alf Müller <4lf-mueller@gmx.de>
2020-08-08 00:18:35 +0200

    Stop teleporting players to players they're not spectating
    If you crouch to stop spectating a player and want to slowly move while free-roaming and hold duck, you are teleported back to the person you were previously spectating. There is no need for this since if you want to return to the person you were spectating, you can just use your reload bind.
    
    Change regarding https://github.com/Facepunch/garrysmod/pull/1696

--- gamemodes/terrortown/gamemode/server/sv_player.lua
+++ gamemodes/terrortown/gamemode/server/sv_player.lua
@@ -379,7 +379,7 @@ function GM:KeyPress(ply, key)
 
 		local target = ply:GetObserverTarget()
 
-		if IsValid(target) and target:IsPlayer() then
+		if IsValid(target) and target:IsPlayer() and ply:GetObserverMode() ~= OBS_MODE_ROAMING then -- Only set the spectator's position to the player they are spectating if they are in chase or eye mode. They can use the reload key if they want to return to the person they're spectating
 			pos = target:EyePos()
 			ang = target:EyeAngles()
 		end
