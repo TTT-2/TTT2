498744e5b66d43f912d1cbf50ea9af209332d2c5
Tim Goll <tim@timgoll.de>
2020-10-02 17:12:06 +0200

    renamed file to a more general name

--- gamemodes/terrortown/gamemode/shared/sh_include.lua
+++ gamemodes/terrortown/gamemode/shared/sh_include.lua
@@ -10,7 +10,7 @@ TTTFiles = {
 	cl_equip = {file = "cl_equip.lua", on = "client"},
 	cl_eventpopup = {file = "cl_eventpopup.lua", on = "client"},
 	cl_fonts = {file = "cl_fonts.lua", on = "client"},
-	cl_help_data = {file = "cl_help_data.lua", on = "client"},
+	cl_menu_data = {file = "cl_menu_data.lua", on = "client"},
 	cl_help = {file = "cl_help.lua", on = "client"},
 	cl_hud_editor = {file = "cl_hud_editor.lua", on = "client"},
 	cl_hud_manager = {file = "cl_hud_manager.lua", on = "client"},
