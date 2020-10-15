1bcfc8d6852309276b68f58087f37d8ebe656b98
NickCloud <nicolasbeiglboeck@gmx.at>
2020-10-15 17:43:04 +0200

    Weapon Pickup: Fixed pickup bug (#672)
    * Weapon Pickup: Fixed pickup bug
    
    Fixed bug where some weapons would stay in inventory even though they should get dropped, leading to a player being able to pickup pretty much infinite weapons.
    
    * Added IsValid checks

--- gamemodes/terrortown/gamemode/server/sv_player_ext.lua
+++ gamemodes/terrortown/gamemode/server/sv_player_ext.lua
@@ -1368,7 +1368,11 @@ function plymeta:SafePickupWeapon(wep, ammoOnly, forcePickup, dropBlockingWeapon
 		-- Very very rarely happens but definitely breaks the weapon and should be avoided at all costs
 		if dropWeapon == wep then return end
 
-		self:SafeDropWeapon(dropWeapon, true)
+		timer.Simple(0, function()
+			if not IsValid(self) or not IsValid(dropWeapon) then return end
+
+			self:SafeDropWeapon(dropWeapon, true)
+		end)
 
 		-- set flag to new weapon that is used to autoselect it later on
 		shouldAutoSelect = shouldAutoSelect or isActiveWeapon
