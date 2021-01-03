863fcf7bee2d895565573ad96bb06787c3f2f075
Histalek <16392835+Histalek@users.noreply.github.com>
2021-01-04 00:19:30 +0100

    LANG: Fix indefinitely rebuilding language settings menu (#706)
    * fix language identifier
    * fix indefinitely rebuilding language settings menu

--- gamemodes/terrortown/gamemode/client/cl_help/populate_language.lua
+++ gamemodes/terrortown/gamemode/client/cl_help/populate_language.lua
@@ -1,5 +1,6 @@
 local materialIcon = Material("vgui/ttt/vskin/helpscreen/language")
 
+local GetActiveLanguageName = LANG.GetActiveLanguageName
 local GetTranslatedLanguageName = LANG.GetTranslatedLanguageName
 local TryT = LANG.TryTranslation
 
@@ -10,21 +11,32 @@ local function PopulateLanguagePanel(parent)
 		label = "label_language_set",
 		convar = "ttt_language",
 		OnChange = function(slf, index, value, rawdata)
+
+			if rawdata == "auto" and GetActiveLanguageName() == "auto" then return end
+
+			if value == GetTranslatedLanguageName(GetActiveLanguageName()) then return end
+
 			vguihandler.InvalidateVSkin()
 			vguihandler.Rebuild()
 		end
 	})
 
 	-- since these are no simple strings, the choices have to be added manually
-	dlang:AddChoice(TryT("lang_server_default"), "auto")
+	dlang:AddChoice(TryT("lang_server_default"), "auto", false)
 
 	for _, lang in pairs(LANG.GetLanguages()) do
-		dlang:AddChoice(GetTranslatedLanguageName(lang), lang)
+		dlang:AddChoice(GetTranslatedLanguageName(lang), lang, false)
+	end
+
+	if GetActiveLanguageName() == "auto" then
+		dlang:ChooseOptionName(TryT("lang_server_default"))
+	else
+		dlang:ChooseOptionName(GetTranslatedLanguageName(GetActiveLanguageName()))
 	end
 
 	form:MakeHelp({
 		label = "help_lang_info",
-		params = {coverage = math.Round(100 * LANG.GetDefaultCoverage(LANG.GetActiveLanguageName()), 1)}
+		params = {coverage = math.Round(100 * LANG.GetDefaultCoverage(GetActiveLanguageName()), 1)}
 	})
 end
 
