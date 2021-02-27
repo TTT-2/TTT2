3aed9bb34085b80a74c434701a73bdc0a58d5a94
Histalek <16392835+Histalek@users.noreply.github.com>
2021-02-27 20:10:39 +0100

    identifier -> namespace

--- lua/ttt2/libraries/migrations.lua
+++ lua/ttt2/libraries/migrations.lua
@@ -14,23 +14,23 @@ local tonumber = tonumber
 local isnumber = isnumber
 
 if not sql.TableExists("migration_master") then
-	sql.Query("CREATE TABLE \"migration_master\" ( \"identifier\" TEXT, \"version\" INTEGER, \"up\" TEXT, \"down\" TEXT, \"created_at\" INTEGER, \"executed_at\" INTEGER, PRIMARY KEY(\"identifier\",\"version\"));")
+	sql.Query("CREATE TABLE \"migration_master\" ( \"namespace\" TEXT, \"version\" INTEGER, \"up\" TEXT, \"down\" TEXT, \"created_at\" INTEGER, \"executed_at\" INTEGER, PRIMARY KEY(\"namespace\",\"version\"));")
 end
 
 ---
 -- Adds a migration to the database. Use @{migrations.MigrateToVersion()} once after all migrations are added.
--- @note This should be used in an extra file at `terrortown/migrations/<realm>/<identifier>.lua`.
--- @param string identifier The unique identifier for a series of migrations.
--- @param number version The unique version in the series of migrations. Should incremented by 1 for each new version.
+-- @note This should be used in an extra file at `terrortown/migrations/<realm>/<namespace>.lua`.
+-- @param string namespace The unique namespace for a series of migrations.
+-- @param number version The unique version in the namespace. Should incremented by 1 for each new version.
 -- @param string upQuery The sqlQuery to execute.
 -- @param [opt]string downQuery The sqlQuery used to revert the `upQuery`.
 -- @param [opt]string existingTable The name of the database table to port to the 'migrations' library.
 -- @return boolean|nil Returns `false` in case of an error and `nil` otherwise.
 -- @realm shared
-function migrations.Add(identifier, version, upQuery, downQuery, existingTable)
+function migrations.Add(namespace, version, upQuery, downQuery, existingTable)
 
 	local checkQuery = "SELECT \"executed_at\" FROM \"migration_master\""
-						.. " WHERE \"identifier\"=" .. sql.SQLStr(identifier)
+						.. " WHERE \"namespace\"=" .. sql.SQLStr(namespace)
 						.. " AND \"version\"=" .. sql.SQLStr(version)
 
 	local checkResult = sql.Query(checkQuery)
@@ -41,8 +41,8 @@ function migrations.Add(identifier, version, upQuery, downQuery, existingTable)
 		return false
 	end
 
-	local masterQuery = "INSERT OR IGNORE INTO \"migration_master\" (\"identifier\", \"version\", \"up\", \"down\", \"created_at\", \"executed_at\") VALUES ("
-						.. sql.SQLStr(identifier) .. ", "
+	local masterQuery = "INSERT OR IGNORE INTO \"migration_master\" (\"namespace\", \"version\", \"up\", \"down\", \"created_at\", \"executed_at\") VALUES ("
+						.. sql.SQLStr(namespace) .. ", "
 						.. sql.SQLStr(version) .. ", "
 						.. sql.SQLStr(upQuery) .. ", "
 						.. sql.SQLStr(downQuery) .. ", "
@@ -56,7 +56,7 @@ function migrations.Add(identifier, version, upQuery, downQuery, existingTable)
 	if not existingTable then return end
 
 	local setExecutedQuery = "UPDATE \"migration_master\" SET \"executed_at\"=" .. sql.SQLStr(os.time())
-						.. " WHERE \"identifier\"=" .. sql.SQLStr(identifier)
+						.. " WHERE \"namespace\"=" .. sql.SQLStr(namespace)
 						.. " AND \"version\"=" .. sql.SQLStr(version)
 
 	if sql.Query(setExecutedQuery) == false then
@@ -65,12 +65,12 @@ function migrations.Add(identifier, version, upQuery, downQuery, existingTable)
 end
 
 ---
--- Runs or reverts migrations with the given identifier to the given version (the state after the upQuery of the specified version).
--- @param string identifier The identifier for the migrations which should be run.
+-- Runs or reverts migrations with the given namespace to the given version (the state after the upQuery of the specified version).
+-- @param string namespace The namespace for the migrations which should be run.
 -- @param number version The desired version of the databaseschema.
 -- @return boolean Returns `true` if the desired version was successfully migrated and `false` in case of an error.
 -- @realm shared
-function migrations.MigrateToVersion(identifier, version)
+function migrations.MigrateToVersion(namespace, version)
 
 	local currentVersion = tonumber(sql.QueryValue("SELECT max(\"version\") FROM \"migration_master\" WHERE \"executed_at\">0"))
 
@@ -80,23 +80,22 @@ function migrations.MigrateToVersion(identifier, version)
 
 	local migrationTable
 
-	if isnumber(currentVersion) and currentVersion > version then -- higher versin than desired -> execute down queries
+	if isnumber(currentVersion) and currentVersion > version then -- higher version than desired -> execute down queries
 		local getMigrationQuery = "SELECT \"version\", \"down\" as query FROM \"migration_master\""
-						.. " WHERE \"identifier\"=" .. sql.SQLStr(identifier)
+						.. " WHERE \"namespace\"=" .. sql.SQLStr(namespace)
 						.. " AND \"version\"<" .. sql.SQLStr(currentVersion)
 						.. " ORDER BY \"version\" DESC"
 
 		migrationTable = sql.Query(getMigrationQuery)
 	else -- lower version or no version -> execute up queries
 		local getMigrationQuery = "SELECT \"version\", \"up\" as query FROM \"migration_master\""
-						.. " WHERE \"identifier\"=" .. sql.SQLStr(identifier)
+						.. " WHERE \"namespace\"=" .. sql.SQLStr(namespace)
 						.. " AND \"version\">" .. sql.SQLStr(currentVersion)
 						.. " ORDER BY \"version\" ASC"
 
 		migrationTable = sql.Query(getMigrationQuery)
 	end
 
-	-- loop through migrationTable and execute queries
 	for _, value in ipairs(migrationTable) do
 
 		sql.Begin()
@@ -106,7 +105,7 @@ function migrations.MigrateToVersion(identifier, version)
 		end
 
 		local setExecutedQuery = "UPDATE \"migration_master\" SET \"executed_at\"=" .. sql.SQLStr(os.time())
-								.. " WHERE \"identifier\"=" .. sql.SQLStr(identifier)
+								.. " WHERE \"namespace\"=" .. sql.SQLStr(namespace)
 								.. " AND \"version\"=" .. sql.SQLStr(version)
 
 		if sql.Query(setExecutedQuery) == false then
