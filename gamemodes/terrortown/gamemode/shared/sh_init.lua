76e41f263c9e52abe0ad61011882fd26e77bb51c
Histalek <16392835+Histalek@users.noreply.github.com>
2021-03-26 16:27:46 +0100

    added changelog, bumped version and small GM-info fixes (#742)

--- gamemodes/terrortown/gamemode/shared/sh_init.lua
+++ gamemodes/terrortown/gamemode/shared/sh_init.lua
@@ -2,10 +2,10 @@
 -- This file contains all shared vars, tables and functions
 
 GM.Name = "TTT2 (Advanced Update)"
-GM.Author = "Bad King Urgrain, Alf21, saibotk, Mineotopia, LeBroomer"
+GM.Author = "Bad King Urgrain, Alf21, saibotk, Mineotopia, LeBroomer, Histalek"
 GM.Email = "ttt2@neoxult.de"
-GM.Website = "ttt.badking.net, ttt2.informaskill.de"
-GM.Version = "0.8.1b"
+GM.Website = "ttt.badking.net, docs.ttt2.neoxult.de"
+GM.Version = "0.8.2b"
 GM.Customized = true
 
 TTT2 = true -- identifier for TTT2. Just use "if TTT2 then ... end"
