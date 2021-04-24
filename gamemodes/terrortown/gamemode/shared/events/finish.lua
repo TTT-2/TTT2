bab393dfc082f72aeb398f6d32b7c635519ba637
Tim Goll <tim@timgoll.de>
2021-04-24 16:30:27 +0200

    only grant bonus score if team won

--- gamemodes/terrortown/gamemode/shared/events/finish.lua
+++ gamemodes/terrortown/gamemode/shared/events/finish.lua
@@ -108,9 +108,9 @@ function EVENT:CalculateScore()
 		end
 
 		self:SetPlayerScore(ply.sid64, {
-			score_alive_teammates = (alive[team] or 0) * roleData.score.aliveTeammatesBonusMultiplier,
+			score_alive_teammates = wintype == team and ((alive[team] or 0) * roleData.score.aliveTeammatesBonusMultiplier) or 0,
 			score_alive_all = aliveAll * roleData.score.allSurviveBonusMultiplier,
-			score_dead_enemies = math.ceil(otherDeadPlayers * roleData.score.surviveBonusMultiplier),
+			score_dead_enemies = wintype == team and math.ceil(otherDeadPlayers * roleData.score.surviveBonusMultiplier) or 0,
 			score_timelimit = wintype == WIN_TIMELIMIT and math.ceil(otherAlivePlayers * roleData.score.timelimitMultiplier) or 0
 		})
 	end
