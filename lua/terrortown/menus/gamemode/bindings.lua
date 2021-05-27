ce8a12b65d31a46fd4f8dbbefc89604c26c77656
Tim Goll <tim@timgoll.de>
2021-05-27 15:39:54 +0200

    added bindings

--- lua/terrortown/menus/gamemode/bindings.lua
+++ lua/terrortown/menus/gamemode/bindings.lua
@@ -2,7 +2,7 @@
 
 CLGAMEMODEMENU.base = "base_gamemodemenu"
 
-CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/addons")
-CLGAMEMODEMENU.title = "menu_addons_title"
-CLGAMEMODEMENU.description = "menu_addons_description"
-CLGAMEMODEMENU.priority = 100
+CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/bindings")
+CLGAMEMODEMENU.title = "menu_bindings_title"
+CLGAMEMODEMENU.description = "menu_bindings_description"
+CLGAMEMODEMENU.priority = 98
---
+++ lua/terrortown/menus/gamemode/bindings/bindings.lua
@@ -0,0 +1,61 @@
+--- @ignore
+
+local function AddBindingCategory(category, parent)
+	local client = LocalPlayer()
+
+	local form = vgui.CreateTTT2Form(parent, category)
+
+	local bindings = bind.GetSettingsBindings()
+
+	for i = 1, #bindings do
+		local binding = bindings[i]
+
+		if binding.category ~= category then continue end
+
+		local currentBinding = bind.Find(binding.name)
+
+		form:MakeBinder({
+			label = binding.label,
+			select = currentBinding,
+			default = binding.defaultKey,
+			OnDisable = function(slf, binder)
+				bind.Remove(currentBinding, binding.name, true)
+
+				binder:SetValue(bind.Find(binding.name))
+			end,
+			OnChange = function(slf, keyNum)
+				if currentBinding == keyNum then return end
+
+				bind.Remove(currentBinding, binding.name, true)
+
+				if keyNum ~= 0 then
+					bind.Add(keyNum, binding.name, true)
+				end
+
+				local key = keyNum ~= 0 and string.upper(input.GetKeyName(keyNum)) or LANG.GetTranslation("button_none")
+
+				client:ChatPrint(
+					LANG.GetParamTranslation("bindings_new", {
+						name = binding.name,
+						key = key
+					})
+				)
+
+				currentBinding = keyNum
+			end
+		})
+	end
+end
+
+CLGAMEMODESUBMENU.base = "base_gamemodesubmenu"
+
+CLGAMEMODESUBMENU.priority = 100
+CLGAMEMODESUBMENU.title = "submenu_bindings_bindings_title"
+
+function CLGAMEMODESUBMENU:Populate(parent)
+	local categories = bind.GetSettingsBindingsCategories()
+
+	for i = 1, #categories do
+		AddBindingCategory(categories[i], parent)
+	end
+end
