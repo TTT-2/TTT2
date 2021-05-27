c4926c6a62a206c92e5aff4ca38fda2cabd368dd
Tim Goll <tim@timgoll.de>
2021-05-27 15:37:08 +0200

    added guide

--- lua/terrortown/menus/gamemode/guide.lua
+++ lua/terrortown/menus/gamemode/guide.lua
@@ -2,7 +2,7 @@
 
 CLGAMEMODEMENU.base = "base_gamemodemenu"
 
-CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/addons")
-CLGAMEMODEMENU.title = "menu_addons_title"
-CLGAMEMODEMENU.description = "menu_addons_description"
-CLGAMEMODEMENU.priority = 100
+CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/guide")
+CLGAMEMODEMENU.title = "menu_guide_title"
+CLGAMEMODEMENU.description = "menu_guide_description"
+CLGAMEMODEMENU.priority = 99
