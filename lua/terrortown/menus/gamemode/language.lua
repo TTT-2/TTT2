0b6ff6f7ee266334ce8099f354b520cdb1f523db
Tim Goll <tim@timgoll.de>
2021-05-27 15:44:00 +0200

    added language

--- lua/terrortown/menus/gamemode/language.lua
+++ lua/terrortown/menus/gamemode/language.lua
@@ -2,7 +2,7 @@
 
 CLGAMEMODEMENU.base = "base_gamemodemenu"
 
-CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/addons")
-CLGAMEMODEMENU.title = "menu_addons_title"
-CLGAMEMODEMENU.description = "menu_addons_description"
-CLGAMEMODEMENU.priority = 100
+CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/language")
+CLGAMEMODEMENU.title = "menu_language_title"
+CLGAMEMODEMENU.description = "menu_language_description"
+CLGAMEMODEMENU.priority = 97
