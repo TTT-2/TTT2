9aec6975e2538e432b00999686a7eb4898059b85
Tim Goll <tim@timgoll.de>
2021-05-27 16:17:02 +0200

    added shops

--- lua/terrortown/menus/gamemode/equipment.lua
+++ lua/terrortown/menus/gamemode/equipment.lua
@@ -2,7 +2,11 @@
 
 CLGAMEMODEMENU.base = "base_gamemodemenu"
 
-CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/addons")
-CLGAMEMODEMENU.title = "menu_addons_title"
-CLGAMEMODEMENU.description = "menu_addons_description"
-CLGAMEMODEMENU.priority = 100
+CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/equipment")
+CLGAMEMODEMENU.title = "menu_equipment_title"
+CLGAMEMODEMENU.description = "menu_equipment_description"
+CLGAMEMODEMENU.priority = 49
+
+function CLGAMEMODEMENU:IsAdminMenu()
+	return true
+end
--- lua/terrortown/menus/gamemode/shops.lua
+++ lua/terrortown/menus/gamemode/shops.lua
@@ -2,7 +2,11 @@
 
 CLGAMEMODEMENU.base = "base_gamemodemenu"
 
-CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/addons")
-CLGAMEMODEMENU.title = "menu_addons_title"
-CLGAMEMODEMENU.description = "menu_addons_description"
-CLGAMEMODEMENU.priority = 100
+CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/shops")
+CLGAMEMODEMENU.title = "menu_shops_title"
+CLGAMEMODEMENU.description = "menu_shops_description"
+CLGAMEMODEMENU.priority = 48
+
+function CLGAMEMODEMENU:IsAdminMenu()
+	return true
+end
