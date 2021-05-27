da5f387bc2ccd1f30e3405bfb507c3f2a4862953
Tim Goll <tim@timgoll.de>
2021-05-27 16:03:01 +0200

    added addons

--- lua/terrortown/menus/gamemode/addons.lua
+++ lua/terrortown/menus/gamemode/addons.lua
@@ -5,4 +5,4 @@ CLGAMEMODEMENU.base = "base_gamemodemenu"
 CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/addons")
 CLGAMEMODEMENU.title = "menu_addons_title"
 CLGAMEMODEMENU.description = "menu_addons_description"
-CLGAMEMODEMENU.priority = 100
+CLGAMEMODEMENU.priority = 94
