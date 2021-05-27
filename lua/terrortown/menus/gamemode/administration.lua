99d33fc8159372374b6741357102d17800a7a80f
Tim Goll <tim@timgoll.de>
2021-05-27 16:15:37 +0200

    added administration

--- lua/terrortown/menus/gamemode/administration.lua
+++ lua/terrortown/menus/gamemode/administration.lua
@@ -2,7 +2,11 @@
 
 CLGAMEMODEMENU.base = "base_gamemodemenu"
 
-CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/addons")
-CLGAMEMODEMENU.title = "menu_addons_title"
-CLGAMEMODEMENU.description = "menu_addons_description"
-CLGAMEMODEMENU.priority = 100
+CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/administration")
+CLGAMEMODEMENU.title = "menu_administration_title"
+CLGAMEMODEMENU.description = "menu_administration_description"
+CLGAMEMODEMENU.priority = 50
+
+function CLGAMEMODEMENU:IsAdminMenu()
+	return true
+end
