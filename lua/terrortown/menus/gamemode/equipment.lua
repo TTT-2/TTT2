e69e680be069c756c409842b86993e155060cc15
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-06-05 00:19:30 +0200

    Sort equipment by language translation
    Only need to fix is #GMOD_MedKit which doesnt get picked up by trytranslation, but for some reason gets translated by the menu-system.

--- lua/terrortown/menus/gamemode/equipment.lua
+++ lua/terrortown/menus/gamemode/equipment.lua
@@ -11,30 +11,48 @@ CLGAMEMODEMENU.title = "menu_equipment_title"
 CLGAMEMODEMENU.description = "menu_equipment_description"
 CLGAMEMODEMENU.priority = 49
 
+CLGAMEMODEMENU.isInitialized = false
+CLGAMEMODEMENU.lang = "en"
+
 function CLGAMEMODEMENU:Initialize()
-	-- Don't create Submenus, as the needed data needs to be loaded later
+	-- Don't create Submenus here as the needed data needs to be loaded later
+	return
 end
 
-function CLGAMEMODEMENU:ReloadData()
+function CLGAMEMODEMENU:InitializeData()
 	-- add "virtual" submenus that are treated as real one even without files
 	virtualSubmenus = {}
 
 	local items = ShopEditor.GetEquipmentForRoleAll()
-	SortEquipmentTable(items)
 	local equipmentMenuBase = self:GetSubmenuByName("base_equipment")
 
+	-- In a first pass get all translated titles
 	for i = 1, #items do
 		local item = items[i]
+		local name = item.EquipMenuData and LANG.TryTranslation(item.EquipMenuData.name)
+		local printName = LANG.TryTranslation(item.PrintName)
+		local title = item.EquipMenuData and name ~= item.EquipMenuData.name and name or printName or item.id or "UNDEFINED"
+		item.title = title
+	end
 
-		virtualSubmenus[i] = tableCopy(equipmentMenuBase)
-		virtualSubmenus[i].title = item.EquipMenuData and item.EquipMenuData.name or item.PrintName or item.id or "UNDEFINED"
-		virtualSubmenus[i].item = item
+	-- In a second pass we insert all items sorted by their translated title
+	local counter = 0
+	for key, item in SortedPairsByMemberValue(items, "title") do
+		counter = counter + 1
+		virtualSubmenus[counter] = tableCopy(equipmentMenuBase)
+		virtualSubmenus[counter].title = item.title
+		virtualSubmenus[counter].item = item
 	end
 end
 
 -- overwrite the normal submenu function to return our custom virtual submenus
 function CLGAMEMODEMENU:GetSubmenus()
-	self:ReloadData()
+	lang = GetConVar("ttt_language"):GetString()
+	if not self.isInitialized or lang ~= self.lang then
+		self.isInitialized = true
+		self.lang = lang
+		self:InitializeData()
+	end
 	return virtualSubmenus
 end
 
