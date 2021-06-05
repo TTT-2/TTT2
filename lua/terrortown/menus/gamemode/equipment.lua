665c486713eb8cf69ab01fc6cd27c3dcccf7681f
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-06-05 16:51:42 +0200

    Fixed and reworked virtual structure

--- lua/terrortown/menus/gamemode/equipment.lua
+++ lua/terrortown/menus/gamemode/equipment.lua
@@ -1,6 +1,7 @@
 --- @ignore
 
 local tableCopy = table.Copy
+local TryT = LANG.TryTranslation
 
 local virtualSubmenus = {}
 
@@ -12,47 +13,60 @@ CLGAMEMODEMENU.description = "menu_equipment_description"
 CLGAMEMODEMENU.priority = 49
 
 CLGAMEMODEMENU.isInitialized = false
-CLGAMEMODEMENU.lang = "en"
+CLGAMEMODEMENU.langConVar = nil
+CLGAMEMODEMENU.lang = nil
 
-function CLGAMEMODEMENU:Initialize()
-	-- Don't create Submenus here as the needed data needs to be loaded later
-	return
-end
-
-function CLGAMEMODEMENU:InitializeData()
+function CLGAMEMODEMENU:InitializeVirtualMenus()
 	-- add "virtual" submenus that are treated as real one even without files
 	virtualSubmenus = {}
 
 	local items = ShopEditor.GetEquipmentForRoleAll()
 	local equipmentMenuBase = self:GetSubmenuByName("base_equipment")
 
-	-- In a first pass get all translated titles
+	-- Assign all items to a virtual menu
+	local counter = 0
 	for i = 1, #items do
 		local item = items[i]
-		local name = item.EquipMenuData and LANG.TryTranslation(item.EquipMenuData.name)
-		local printName = LANG.TryTranslation(item.PrintName)
-		local title = item.EquipMenuData and name ~= item.EquipMenuData.name and name or printName or item.id or "UNDEFINED"
-		item.title = title
-	end
 
-	-- In a second pass we insert all items sorted by their translated title
-	local counter = 0
-	for key, item in SortedPairsByMemberValue(items, "title") do
+		-- Only keep ttt-equipments that are cached
+		if not item.ttt2_cached_material and not item.ttt2_cached_model then continue end
 		counter = counter + 1
+
 		virtualSubmenus[counter] = tableCopy(equipmentMenuBase)
-		virtualSubmenus[counter].title = item.title
 		virtualSubmenus[counter].item = item
 	end
 end
 
+function CLGAMEMODEMENU:TranslateAndSortMenus()
+	-- In a first Pass translate all item names and assign them to a title
+	for _, vMenu in ipairs(virtualSubmenus) do
+		local item = vMenu.item
+		local name = item.EquipMenuData and TryT(item.EquipMenuData.name)
+
+		vMenu.title = item.EquipMenuData and name ~= item.EquipMenuData.name and name or TryT(item.PrintName) or item.id or "UNDEFINED"
+	end
+
+	-- In a second pass we sort by their translated title
+	table.SortByMember(virtualSubmenus,"title",true)
+end
+
 -- overwrite the normal submenu function to return our custom virtual submenus
 function CLGAMEMODEMENU:GetSubmenus()
-	lang = GetConVar("ttt_language"):GetString()
-	if not self.isInitialized or lang ~= self.lang then
+	if not self.isInitialized then
 		self.isInitialized = true
-		self.lang = lang
-		self:InitializeData()
+		self.langConVar = GetConVar("ttt_language")
+
+		self:InitializeVirtualMenus()
 	end
+
+	local lang = self.langConVar:GetString()
+
+	if lang ~= self.lang then
+		self.lang = lang
+
+		self:TranslateAndSortMenus()
+	end
+
 	return virtualSubmenus
 end
 
