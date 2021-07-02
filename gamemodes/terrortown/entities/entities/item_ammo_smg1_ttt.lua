55eb725d8a53b0423471326c0b2dabc62b9343d8
Tim Goll <tim@timgoll.de>
2021-07-02 11:00:41 +0200

    added ammo ents

---
+++ gamemodes/terrortown/entities/entities/item_ammo_smg1_ttt.lua
@@ -0,0 +1,11 @@
+if SERVER then
+	AddCSLuaFile()
+end
+
+ENT.Type = "anim"
+ENT.Base = "base_ammo_ttt"
+ENT.AmmoType = "SMG1"
+ENT.AmmoAmount = 30
+ENT.AmmoMax = 60
+ENT.Model = Model("models/items/boxmrounds.mdl")
+ENT.AutoSpawnable = true
