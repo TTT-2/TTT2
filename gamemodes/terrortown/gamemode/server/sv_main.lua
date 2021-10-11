84a19d4caa82f1f0a85fc03d743bd6992805b634
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-10-11 19:17:29 +0200

    Later Initialize

--- gamemodes/terrortown/gamemode/server/sv_main.lua
+++ gamemodes/terrortown/gamemode/server/sv_main.lua
@@ -317,9 +317,6 @@ function GM:Initialize()
 	ShopEditor.SetupShopEditorCVars()
 	ShopEditor.CreateShopDBs()
 
-	-- initialize playermodel database
-	playermodels.Initialize()
-
 	-- Force friendly fire to be enabled. If it is off, we do not get lag compensation.
 	RunConsoleCommand("mp_friendlyfire", "1")
 
@@ -529,6 +526,9 @@ function GM:InitPostEntity()
 
 	-- precache player models
 	playermodels.PrecacheModels()
+
+	-- initialize playermodel database
+	playermodels.Initialize()
 
 	timer.Simple(0, function()
 		addonChecker.Check()
