759facaea6e431ef028a9b297b2526e760b582b9
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-10-16 18:30:06 +0200

    Delete all clientside gui code

--- gamemodes/terrortown/gamemode/server/sv_shopeditor.lua
+++ gamemodes/terrortown/gamemode/server/sv_shopeditor.lua
@@ -5,7 +5,6 @@ local net = net
 local sql = sql
 local util = util
 
-util.AddNetworkString("newshop")
 util.AddNetworkString("TTT2UpdateCVar")
 
 net.Receive("TTT2UpdateCVar", function(_, ply)
@@ -19,23 +18,6 @@ net.Receive("TTT2UpdateCVar", function(_, ply)
 end)
 
 ShopEditor.ShopTablePre = "ttt2_shop_"
-
----
--- Opens the ShopEditor for a specific @{Player}
--- @note Just admins are allowed to do so!
--- @param Player ply
--- @param string cmd
--- @param any args
--- @realm server
-function ShopEditor.ShopEditor(ply, cmd, args)
-	if ply:IsAdmin() then
-		net.Start("newshop")
-		net.Send(ply)
-	else
-		ply:ChatPrint("[TTT2][INFO] You need to be an admin to access the ShopEditor!")
-	end
-end
-concommand.Add("shopeditor", ShopEditor.ShopEditor)
 
 ---
 -- Initializes the SQL database for the ShopEditor
