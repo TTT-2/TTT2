32a4b68375193e55cdadaac6b0adb80dc559ac1d
Tim Goll <tim@timgoll.de>
2021-11-09 15:51:33 +0100

    added toggleable HUD elements

---
+++ lua/terrortown/menus/gamemode/administration/mapentities.lua
@@ -0,0 +1,10 @@
+--- @ignore
+
+CLGAMEMODESUBMENU.base = "base_gamemodesubmenu"
+
+CLGAMEMODESUBMENU.priority = 91
+CLGAMEMODESUBMENU.title = "submenu_administration_mapentities_title"
+
+function CLGAMEMODESUBMENU:Populate(parent)
+
+end
