e1344a7572fe51c5d04bc96a8334987df9566cc8
Tim Goll <tim@timgoll.de>
2021-11-09 14:36:43 +0100

    added dead player settings

--- lua/terrortown/menus/gamemode/administration/roundsetup.lua
+++ lua/terrortown/menus/gamemode/administration/roundsetup.lua
@@ -24,19 +24,51 @@ function CLGAMEMODESUBMENU:Populate(parent)
 		label = "help_hint_armor"
 	})
 
-	local form2 = vgui.CreateTTT2Form(parent, "header_round_setup_prep")
+	local form2 = vgui.CreateTTT2Form(parent, "header_round_dead_players")
 
 	form2:MakeCheckBox({
+		serverConvar = "ttt_identify_body_woconfirm",
+		label = "label_identify_body_woconfirm"
+	})
+
+	form2:MakeCheckBox({
+		serverConvar = "ttt_announce_body_found",
+		label = "label_announce_body_found"
+	})
+
+	form2:MakeCheckBox({
+		serverConvar = "ttt2_confirm_killlist",
+		label = "label_confirm_killlist"
+	})
+
+	form2:MakeCheckBox({
+		serverConvar = "ttt2_inspect_detective_only",
+		label = "label_inspect_detective_only"
+	})
+
+	form2:MakeCheckBox({
+		serverConvar = "ttt2_confirm_detective_only",
+		label = "label_confirm_detective_only"
+	})
+
+	form2:MakeCheckBox({
+		serverConvar = "ttt_dyingshot",
+		label = "label_dyingshot"
+	})
+
+	local form3 = vgui.CreateTTT2Form(parent, "header_round_setup_prep")
+
+	form3:MakeCheckBox({
 		serverConvar = "ttt2_prep_respawn",
 		label = "label_prep_respawn"
 	})
 
-	form2:MakeCheckBox({
+	form3:MakeCheckBox({
 		serverConvar = "ttt_nade_throw_during_prep",
 		label = "label_nade_throw_during_prep"
 	})
 
-	form2:MakeSlider({
+	form3:MakeSlider({
 		serverConvar = "ttt_preptime_seconds",
 		label = "label_preptime_seconds",
 		min = 0,
@@ -44,7 +76,7 @@ function CLGAMEMODESUBMENU:Populate(parent)
 		decimal = 0
 	})
 
-	form2:MakeSlider({
+	form3:MakeSlider({
 		serverConvar = "ttt_firstpreptime",
 		label = "label_firstpreptime_seconds",
 		min = 0,
@@ -52,9 +84,9 @@ function CLGAMEMODESUBMENU:Populate(parent)
 		decimal = 0
 	})
 
-	local form3 = vgui.CreateTTT2Form(parent, "header_round_setup_round")
+	local form4 = vgui.CreateTTT2Form(parent, "header_round_setup_round")
 
-	form3:MakeSlider({
+	form4:MakeSlider({
 		serverConvar = "ttt_roundtime_minutes",
 		label = "label_roundtime_minutes",
 		min = 0,
@@ -62,16 +94,16 @@ function CLGAMEMODESUBMENU:Populate(parent)
 		decimal = 0,
 	})
 
-	form3:MakeHelp({
+	form4:MakeHelp({
 		label = "help_haste_mode"
 	})
 
-	local enbHaste = form3:MakeCheckBox({
+	local enbHaste = form4:MakeCheckBox({
 		serverConvar = "ttt_haste",
 		label = "label_haste"
 	})
 
-	form3:MakeSlider({
+	form4:MakeSlider({
 		serverConvar = "ttt_haste_starting_minutes",
 		label = "label_haste_starting_minutes",
 		min = 0,
@@ -80,7 +112,7 @@ function CLGAMEMODESUBMENU:Populate(parent)
 		master = enbHaste
 	})
 
-	form3:MakeSlider({
+	form4:MakeSlider({
 		serverConvar = "ttt_haste_minutes_per_death",
 		label = "label_haste_minutes_per_death",
 		min = 0,
@@ -89,14 +121,14 @@ function CLGAMEMODESUBMENU:Populate(parent)
 		master = enbHaste
 	})
 
-	local form4 = vgui.CreateTTT2Form(parent, "header_round_setup_post")
+	local form5 = vgui.CreateTTT2Form(parent, "header_round_setup_post")
 
-	form4:MakeCheckBox({
+	form5:MakeCheckBox({
 		serverConvar = "ttt_postround_dm",
 		label = "label_postround_dm"
 	})
 
-	form4:MakeSlider({
+	form5:MakeSlider({
 		serverConvar = "ttt_posttime_seconds",
 		label = "label_posttime_seconds",
 		min = 0,
@@ -104,13 +136,13 @@ function CLGAMEMODESUBMENU:Populate(parent)
 		decimal = 0
 	})
 
-	local form5 = vgui.CreateTTT2Form(parent, "header_round_setup_map_duration")
+	local form6 = vgui.CreateTTT2Form(parent, "header_round_setup_map_duration")
 
-	form5:MakeHelp({
+	form6:MakeHelp({
 		label = "help_round_limit"
 	})
 
-	form5:MakeSlider({
+	form6:MakeSlider({
 		serverConvar = "ttt_round_limit",
 		label = "label_round_limit",
 		min = 0,
@@ -118,7 +150,7 @@ function CLGAMEMODESUBMENU:Populate(parent)
 		decimal = 0
 	})
 
-	form5:MakeSlider({
+	form6:MakeSlider({
 		serverConvar = "ttt_time_limit_minutes",
 		label = "label_time_limit_minutes",
 		min = 0,
