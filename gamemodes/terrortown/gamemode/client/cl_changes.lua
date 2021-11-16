c18573059c2c54fb40aea7058b10c2cb34ec6ff4
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-11-16 18:53:45 +0100

    Added latest PRs

--- gamemodes/terrortown/gamemode/client/cl_changes.lua
+++ gamemodes/terrortown/gamemode/client/cl_changes.lua
@@ -1197,6 +1197,8 @@ function CreateChanges()
 		<ul>
 			<li>Fixed <code>ply:Give(weapon)</code> to work again, when weapons are cached, fixing the spawneditor to work again</li>
 			<li>Fixed spawneditor not causing errors, when going through walls due to many steps</li>
+			<li>Set default traitor button variable back to 0</li>
+			<li>Fixed unchanged or unscaled damage being sent to the client, leading to a wrongly working damage-overlay</li>
 		</ul>
 	]], os.time({ year = 2021, month = 11, day = 16 }))
 
