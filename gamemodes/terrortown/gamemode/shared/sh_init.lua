ca7c37cc32e15e1b97bb2550f0aeed752f602be0
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-11-18 17:33:28 +0100

    Release v0.11.3b (#940)

--- gamemodes/terrortown/gamemode/shared/sh_init.lua
+++ gamemodes/terrortown/gamemode/shared/sh_init.lua
@@ -5,7 +5,7 @@ GM.Name = "TTT2 (Advanced Update)"
 GM.Author = "Bad King Urgrain, Alf21, saibotk, Mineotopia, LeBroomer, Histalek, ZenBre4ker"
 GM.Email = "ttt2@neoxult.de"
 GM.Website = "ttt.badking.net, docs.ttt2.neoxult.de"
-GM.Version = "0.11.2b"
+GM.Version = "0.11.3b"
 GM.Customized = true
 
 TTT2 = true -- identifier for TTT2. Just use "if TTT2 then ... end"
