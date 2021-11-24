30305326b181ffbd42309a0ff535b76f8c655beb
ZenBre4ker <72046466+ZenBre4ker@users.noreply.github.com>
2021-11-24 15:32:56 +0100

    Rework language menu for new Dropdown

--- lua/terrortown/menus/gamemode/language/language.lua
+++ lua/terrortown/menus/gamemode/language/language.lua
@@ -12,12 +12,21 @@ CLGAMEMODESUBMENU.title = "submenu_language_language_title"
 function CLGAMEMODESUBMENU:Populate(parent)
 	local form = vgui.CreateTTT2Form(parent, "header_language")
 
-	local dlang = form:MakeComboBox({
+	local choices = {}
+	local activeLanguageName = GetActiveLanguageName()
+
+	choices[1] = {title = TryT("lang_server_default"), value = "auto", select = activeLanguageName == "auto"}
+
+	for _, lang in pairs(LANG.GetLanguages()) do
+		choices[#choices + 1] = {title = GetTranslatedLanguageName(lang), value = lang, select =  activeLanguageName == lang}
+	end
+
+	form:MakeComboBox({
 		label = "label_language_set",
 		convar = "ttt_language",
-		OnChange = function(slf, index, value, rawdata)
-
-			if rawdata == "auto" and GetActiveLanguageName() == "auto" then return end
+		choices = choices,
+		OnChange = function(value)
+			if value == "auto" and GetActiveLanguageName() == "auto" then return end
 
 			if value == GetTranslatedLanguageName(GetActiveLanguageName()) then return end
 
@@ -26,21 +35,8 @@ function CLGAMEMODESUBMENU:Populate(parent)
 		end
 	})
 
-	-- since these are no simple strings, the choices have to be added manually
-	dlang:AddChoice(TryT("lang_server_default"), "auto", false)
-
-	for _, lang in pairs(LANG.GetLanguages()) do
-		dlang:AddChoice(GetTranslatedLanguageName(lang), lang, false)
-	end
-
-	if GetActiveLanguageName() == "auto" then
-		dlang:ChooseOptionName(TryT("lang_server_default"))
-	else
-		dlang:ChooseOptionName(GetTranslatedLanguageName(GetActiveLanguageName()))
-	end
-
 	form:MakeHelp({
 		label = "help_lang_info",
-		params = {coverage = math.Round(100 * LANG.GetDefaultCoverage(GetActiveLanguageName()), 1)}
+		params = {coverage = math.Round(100 * LANG.GetDefaultCoverage(activeLanguageName), 1)}
 	})
 end
