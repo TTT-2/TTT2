13d74da3012a0eefa650906b733d4c0268b6596f
Jane <margjane@keemail.me>
2022-04-21 11:45:00 +0800

    fix ammo blocking +use traces
    https://redd.it/sxieur

--- gamemodes/terrortown/entities/entities/base_ammo_ttt.lua
+++ gamemodes/terrortown/entities/entities/base_ammo_ttt.lua
@@ -36,17 +36,11 @@ function ENT:Initialize()
 
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_VPHYSICS)
-	self:SetSolid(SOLID_BBOX)
+	self:AddSolidFlags(FSOLID_TRIGGER)
 
 	self:SetCollisionGroup(COLLISION_GROUP_WEAPON)
 
-	local b = 26
-
-	self:SetCollisionBounds(Vector(-b, -b, -b), Vector(b, b, b))
-
-	if SERVER then
-		self:SetTrigger(true)
-	end
+	self:UseTriggerBounds(true, 24)
 
 	self.tickRemoval = false
 	self.AmmoEntMax = self.AmmoAmount
