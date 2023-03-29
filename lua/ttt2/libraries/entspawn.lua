8e22139c2e62092cd7a99425040a735fc0b55f71
波と一緒に漂う <2247564152@qq.com>
2023-03-29 20:07:12 +0800

    Add files via upload

--- lua/ttt2/libraries/entspawn.lua
+++ lua/ttt2/libraries/entspawn.lua
@@ -140,10 +140,10 @@ function entspawn.SpawnEntities(spawns, entsForTypes, entTable, randomType)
 	for entType, spawnTable in pairs(spawns) do
 		for i = 1, #spawnTable do
 			local spawn = spawnTable[i]
+			
 			--Check if spawn.pos is valid
-			if not spawn or not spawn.pos then
-				continue
-			end
+			if not spawn or not spawn.pos then continue end
+			
 			-- if the weapon spawn is a random weapon spawn, select any spawnable weapon
 			local selectedEnt
 			if entType == randomType then
@@ -180,7 +180,7 @@ function entspawn.SpawnEntities(spawns, entsForTypes, entTable, randomType)
 				ammoEnt:Spawn()
 				ammoEnt:PhysWake()
 			end
-		end
+	    end
 	end
 end
 ---
