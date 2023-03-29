1da8f168abf9a88cbc2d39fc957ce5764b34d462
波と一緒に漂う <2247564152@qq.com>
2023-03-29 20:07:34 +0800

    Add files via upload

--- gamemodes/terrortown/entities/weapons/weapon_ttt_spawneditor.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_spawneditor.lua
@@ -177,10 +177,10 @@ if CLIENT then
 			for i = 1, #spawns do
 				local spawn = spawns[i]
 				local pos = spawn.pos
+
 				--If pos is nil, continue to the next iteration
-                if not pos then 
-				    continue 
-				end
+                if not pos then continue end
+
 				-- the screenPos has to be calculatet inside a non modified cam3D space
 				-- to yield correct results
 				camStart3D()
