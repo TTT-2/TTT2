c9798752fd17157bdf8837b3db37f5fa2b32adff
EntranceJew <EntranceJew@gmail.com>
2023-10-27 18:19:45 -0500

    Revert "use global vectors, especially inside those loops"
    This reverts commit 917d9c1c90651971eb71754bb6cf3bdd5745b5a4.
    
    # Conflicts:
    #	CHANGELOG.md

--- gamemodes/terrortown/entities/weapons/weapon_zm_pistol.lua
+++ gamemodes/terrortown/entities/weapons/weapon_zm_pistol.lua
@@ -40,4 +40,4 @@ SWEP.ViewModel = "models/weapons/cstrike/c_pist_fiveseven.mdl"
 SWEP.WorldModel = "models/weapons/w_pist_fiveseven.mdl"
 
 SWEP.IronSightsPos = Vector(-5.95, -4, 2.799)
-SWEP.IronSightsAng = vector_origin
+SWEP.IronSightsAng = Vector(0, 0, 0)
