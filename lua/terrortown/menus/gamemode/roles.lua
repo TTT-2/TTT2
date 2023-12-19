a4c3cf8db34365dcd0af1936494ff98258e6f739
EntranceJew <EntranceJew@gmail.com>
2023-12-19 05:33:53 -0600

    roles builtin

--- lua/terrortown/menus/gamemode/roles.lua
+++ lua/terrortown/menus/gamemode/roles.lua
@@ -15,6 +15,8 @@ CLGAMEMODEMENU.priority = 47
 CLGAMEMODEMENU.isInitialized = false
 CLGAMEMODEMENU.roles = nil
 
+local builtinIcon = Material("vgui/ttt/vskin/markers/builtin")
+
 function CLGAMEMODEMENU:IsAdminMenu()
 	return true
 end
@@ -36,6 +38,7 @@ function CLGAMEMODEMENU:InitializeVirtualMenus()
 		virtualSubmenus[counter].title = roleData.name
 		virtualSubmenus[counter].icon = roleData.iconMaterial
 		virtualSubmenus[counter].roleData = roleData
+		virtualSubmenus[counter].iconBadge = roleData.builtin and builtinIcon
 		virtualSubmenus[counter].basemenu = self
 	end
 end
