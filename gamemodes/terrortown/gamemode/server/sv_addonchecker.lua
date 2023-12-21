00424042a662a9b54e1f48f56f2ed5b1c6c8a899
Sascha Moser <28950736+xopez@users.noreply.github.com>
2023-12-21 11:23:30 +0100

    Include fixed version of the Death Note
    - Roles are hardcoded
    - In TTT2 all addons print only detective roles
    - The new addon prints all roles except the own teamrole no matter for which team they play

--- gamemodes/terrortown/gamemode/server/sv_addonchecker.lua
+++ gamemodes/terrortown/gamemode/server/sv_addonchecker.lua
@@ -194,6 +194,21 @@ addonChecker.curatedList = {
 		reason = "Overwrites stock TTT2 crowbar which chauses problems with doors and ttt_map_settings entity.",
 		type = ADDON_INCOMPATIBLE,
 	},
+	["278185787"] = { -- Death Note by Blue-Pentagram
+		alternative = "3118796974",
+		reason = "Prints only Detectives in TTT2. Roles are hard coded.",
+		type = ADDON_INCOMPATIBLE
+	},
+	["2758610950"] = { -- Death Note TTT2 Fixed Edition	by pat201290
+		alternative = "3118796974",
+		reason = "Prints only Detectives in TTT2. Roles are hard coded.",
+		type = ADDON_INCOMPATIBLE
+	},
+	["110148946"] = { -- Death Note by SmokeTheBanana
+		alternative = "3118796974",
+		reason = "Prints only Detectives in TTT2. Roles are hard coded.",
+		type = ADDON_INCOMPATIBLE
+	},
 	["456247192"] = { -- TTT Coffee-Cup Hunt by Niandra!
 		alternative = "2150924507",
 		reason = "Addon is broken and doesn't do anything.",
