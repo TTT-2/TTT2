ad810de144e8502c991ab814d099fa495fe665f1
Tim Goll <github@timgoll.de>
2024-01-14 20:52:45 +0100

    fixed docs

--- lua/ttt2/extensions/player.lua
+++ lua/ttt2/extensions/player.lua
@@ -12,8 +12,8 @@ if SERVER then
 	---
 	-- Registers a synced client setting on the server. This has to be done so that settings set by
 	-- `ply:SetSettingOnServer` are not discarded on the server.
-	-- @parameter string identifier The identifier of the synced setting
-	-- @parameter string type The data type of the setting, e.g. `number`, `bool` or `string`
+	-- @param string identifier The identifier of the synced setting
+	-- @param string type The data type of the setting, e.g. `number`, `bool` or `string`
 	-- @realm server
 	function player.RegisterSettingOnServer(identifier, type)
 		player.playerSettingRegistry[identifier] = type
