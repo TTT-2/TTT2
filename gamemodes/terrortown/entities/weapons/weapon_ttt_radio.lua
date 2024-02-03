e145b6b7e0f69b2abd0194f9103459b67bd82db6
Tim Goll <github@timgoll.de>
2024-02-03 10:02:46 +0100

    forgot the radio

--- gamemodes/terrortown/entities/weapons/weapon_ttt_radio.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_radio.lua
@@ -13,8 +13,7 @@ if CLIENT then
 	SWEP.PrintName = "radio_name"
 	SWEP.Slot = 7
 
-	SWEP.ViewModelFlip = false
-	SWEP.ViewModelFOV = 10
+	SWEP.ShowDefaultViewModel = false
 
 	SWEP.EquipMenuData = {
 		type = "item_weapon",
@@ -50,7 +49,6 @@ SWEP.builtin = true
 
 SWEP.AllowDrop = false
 SWEP.NoSights = true
-SWEP.ShowDefaultViewModel = true
 
 ---
 -- @ignore
