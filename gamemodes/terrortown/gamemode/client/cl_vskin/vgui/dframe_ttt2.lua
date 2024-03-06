01b3df5e6173ab51901f3f1322454964b646640f
Tim Goll <github@timgoll.de>
2024-03-06 23:30:29 +0100

    added sounds to F1 menu

--- gamemodes/terrortown/gamemode/client/cl_vskin/vgui/dframe_ttt2.lua
+++ gamemodes/terrortown/gamemode/client/cl_vskin/vgui/dframe_ttt2.lua
@@ -218,7 +218,7 @@ function PANEL:OnClear() end
 -- @realm client
 function PANEL:InitButtons()
     -- add close button
-    self.btnClose = vgui.Create("DButton", self)
+    self.btnClose = vgui.Create("DButtonTTT2", self)
     self.btnClose:SetText("")
     self.btnClose:SetVisible(true)
 
@@ -231,7 +231,7 @@ function PANEL:InitButtons()
     end
 
     -- add back button
-    self.btnBack = vgui.Create("DButton", self)
+    self.btnBack = vgui.Create("DButtonTTT2", self)
     self.btnBack:SetText("")
     self.btnBack:SetVisible(true)
 
