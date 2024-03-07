95d8f63c7bd35ff51d60751899fd03c1456a429d
Tim Goll <github@timgoll.de>
2024-03-07 23:23:08 +0100

    Update en.lua

--- lua/terrortown/lang/en.lua
+++ lua/terrortown/lang/en.lua
@@ -2192,14 +2192,14 @@ L.throw_no_room = "You have no space here to throw this device"
 -- 2024-03-04
 L.use_entity = "Press [{usekey}] to use"
 
--- 2024-03.06
+-- 2024-03-06
 L.submenu_gameplay_sounds_title = "Client-Sounds"
 
 L.header_sounds_settings = "UI Sound Settings"
 
 L.help_enable_sound_interact = "Interaction sounds are those sounds that are played when opening an UI. Such a sound is played for example when interacting with the radio marker."
 L.help_enable_sound_buttons = "Button sounds are clicky sounds that are played when clicking a button."
-L.help_enable_sound_message = "Message or notification sounds are played for chat and mStack messages. They can be quite obnoxious."
+L.help_enable_sound_message = "Message or notification sounds are played for chat mesages and notifications. They can be quite obnoxious."
 
 L.label_enable_sound_interact = "Enable interaction sounds"
 L.label_enable_sound_buttons = "Enable button sounds"
