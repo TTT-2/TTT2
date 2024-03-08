05ea80a15fc2832b057536d6ef5a52bbc078f2a0
TW1STaL1CKY <TW1STaL1CKY@users.noreply.github.com>
2024-03-08 09:09:25 +0000

    Bugfix: Fixed issue with +use for traitors (#1461)

--- gamemodes/terrortown/gamemode/client/cl_tbuttons.lua
+++ gamemodes/terrortown/gamemode/client/cl_tbuttons.lua
@@ -77,6 +77,7 @@ function TBHUD:PlayerIsFocused()
         and ply:IsActive()
         and self.focus_but
         and (self.focus_but.access or self.focus_but.admin)
+        and self.focus_stick >= CurTime()
         and IsValid(self.focus_but.ent)
 end
 
