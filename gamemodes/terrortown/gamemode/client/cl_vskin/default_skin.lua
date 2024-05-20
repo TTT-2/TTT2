TTT-2/TTT2

commit 2e6ae9197d2e7e81e892ca0cb0e353503416feb0

UI: Adding new panel and extra buttons (#1509)
To support more features / actions in the F1 menu, additional UI
elements have to be added. This is a PR that aims to bring the elements
to the panels:


![image](https://github.com/TTT-2/TTT2/assets/13639408/db095e63-095a-4268-8965-f53555ccdcc8)
This is a simple button that can have any action attached


![image](https://github.com/TTT-2/TTT2/assets/13639408/fe34c875-660c-40c2-b7d1-388395a89675)
These are two new types of buttons that can be added to many elemens. A
"run" button that is basically a normal button that performs an action
and a "toggle" button that toggles between states. Both can have any
color or any icon, shown here are the default colors and states.

This is how these elements are added:
```lua
local form2 = vgui.CreateTTT2Form(parent, "new UI elements")

form2:MakeButton({
    label = "some label",
    buttonLabel = "this button is clickable",
    OnClick = function(slf)
        print("I was pressed")
    end,
})

form2:MakeCheckBox({
    label = "generic convar with two extra buttons",
    convar = "ttt2b_grenade_trajectory_ui",
    enableToggle = true,
    enableRun = true,
    OnClickToggle = function(button, state)
        print("toggling", button, state)
    end,
    OnClickRun = function(button)
        print("running", button)
    end,
})

form2:MakeCheckBox({
    label = "generic convar with two extra buttons number two",
    convar = "ttt2c_grenade_trajectory_ui",
    enableToggle = true,
    enableRun = true,
    toggleInitialState = 2,
    OnClickToggle = function(button, state)
        print("toggling", button, state)
    end,
    OnClickRun = function(button)
        print("running", button)
    end,
})

form2:MakeCheckBox({
    label = "only a toggle button!",
    convar = "ttt2c_grenade_trajectory_ui",
    enableToggle = true,
    OnClickToggle = function(button, state)
        print("toggling", button, state)
    end,
})

gamemodes/terrortown/gamemode/client/cl_vskin/default_skin.lua
@@ -716,18 +716,75 @@ end
 -- @param number h
 -- @realm client
 function SKIN:PaintFormButtonIconTTT2(panel, w, h)
-    local colorBoxBack = colors.settingsBox
     local colorBox = colors.accent
+
+    if panel.colorBackground then
+        if IsColor(panel.colorBackground) then
+            colorBox = panel.colorBackground
+        else
+            colorBox = panel.colorBackground[panel.state or 1]
+        end
+    end
+
+    local iconMaterial = panel.iconMaterial
+
+    if not iconMaterial.GetTexture then
+        iconMaterial = iconMaterial[panel.state or 1]
+    end
+
+    local colorBoxBack = colors.settingsBox
     local colorText = ColorAlpha(utilGetDefaultColor(colors.accent), 150)
     local shift = 0
     local pad = 6
 
     if not panel:IsEnabled() then
-        colorBoxBack = ColorAlpha(colors.settingsBox, alphaDisabled)
-        colorBox = ColorAlpha(colors.accent, alphaDisabled)
-        colorText = ColorAlpha(utilGetDefaultColor(colors.accent), alphaDisabled)
+        colorBoxBack = ColorAlpha(colorBoxBack, alphaDisabled)
+        colorBox = ColorAlpha(colorBox, alphaDisabled)
+        colorText = ColorAlpha(utilGetDefaultColor(colorBox), alphaDisabled)
     elseif panel.noDefault then
-        colorBoxBack = colors.settingsBox
+        colorBox = ColorAlpha(colorBox, alphaDisabled)
+        colorText = ColorAlpha(utilGetDefaultColor(colorBox), alphaDisabled)
+    elseif panel.Depressed or panel:IsSelected() or panel:GetToggle() then
+        colorBox = utilGetActiveColor(colorBox)
+        colorText = ColorAlpha(utilGetDefaultColor(colorBox), 150)
+        shift = 1
+    elseif panel.Hovered then
+        colorBox = utilGetHoverColor(colorBox)
+        colorText = ColorAlpha(utilGetDefaultColor(colorBox), 150)
+    end
+
+    if panel.roundedCorner then
+        drawRoundedBoxEx(sizes.cornerRadius, 0, 0, w, h, colorBoxBack, false, true, false, true)
+    else
+        drawBox(0, 0, w, h, colorBoxBack)
+    end
+
+    drawRoundedBox(sizes.cornerRadius, 1, 1, w - 2, h - 2, colorBox)
+
+    drawFilteredShadowedTexture(
+        pad,
+        pad + shift,
+        w - 2 * pad,
+        h - 2 * pad,
+        iconMaterial,
+        colorText.a,
+        colorText
+    )
+end
+
+---
+-- @param Panel panel
+-- @param number w
+-- @param number h
+-- @realm client
+function SKIN:PaintFormButtonTTT2(panel, w, h)
+    local colorBoxBack = colors.settingsBox
+    local colorBox = colors.accent
+    local colorText = ColorAlpha(utilGetDefaultColor(colors.accent), 150)
+    local shift = 0
+
+    if not panel:IsEnabled() then
+        colorBoxBack = ColorAlpha(colors.settingsBox, alphaDisabled)
         colorBox = ColorAlpha(colors.accent, alphaDisabled)
         colorText = ColorAlpha(utilGetDefaultColor(colors.accent), alphaDisabled)
     elseif panel.Depressed or panel:IsSelected() or panel:GetToggle() then
@@ -744,14 +801,14 @@ function SKIN:PaintFormButtonIconTTT2(panel, w, h)
     drawRoundedBoxEx(sizes.cornerRadius, 0, 0, w, h, colorBoxBack, false, true, false, true)
     drawRoundedBox(sizes.cornerRadius, 1, 1, w - 2, h - 2, colorBox)
 
-    drawFilteredShadowedTexture(
-        pad,
-        pad + shift,
-        w - 2 * pad,
-        h - 2 * pad,
-        panel.material,
-        colorText.a,
-        colorText
+    drawShadowedText(
+        TryT(panel:GetText()),
+        panel:GetFont(),
+        0.5 * w,
+        0.5 * h + shift,
+        colorText,
+        TEXT_ALIGN_CENTER,
+        TEXT_ALIGN_CENTER
     )
 end
 

gamemodes/terrortown/gamemode/client/cl_vskin/vgui/dform_ttt2.lua
@@ -22,6 +22,9 @@ AccessorFunc(PANEL, "m_iSpacing", "Spacing")
 AccessorFunc(PANEL, "m_Padding", "Padding")
 
 local materialReset = Material("vgui/ttt/vskin/icon_reset")
+local materialCheckmark = Material("vgui/ttt/vskin/icon_checkmark")
+local materialCross = Material("vgui/ttt/vskin/icon_cross")
+local materialRun = Material("vgui/ttt/vskin/icon_run")
 local materialDisable = Material("vgui/ttt/vskin/icon_disable")
 
 ---
@@ -64,9 +67,11 @@ end
 ---
 -- @param Panel left
 -- @param Panel right
--- @param Panel reset
+-- @param Panel buttonReset
+-- @param Panel buttonToggle
+-- @param Panel buttonRun
 -- @realm client
-function PANEL:AddItem(left, right, reset)
+function PANEL:AddItem(left, right, buttonReset, buttonToggle, buttonRun)
     self:InvalidateLayout()
 
     local panel = vgui.Create("DSizeToContents", self)
@@ -76,9 +81,19 @@ function PANEL:AddItem(left, right, reset)
     panel:DockPadding(10, 10, 10, 0)
     panel:InvalidateLayout()
 
-    if IsValid(reset) then
-        reset:SetParent(panel)
-        reset:Dock(RIGHT)
+    if IsValid(buttonReset) then
+        buttonReset:SetParent(panel)
+        buttonReset:Dock(RIGHT)
+    end
+
+    if IsValid(buttonToggle) then
+        buttonToggle:SetParent(panel)
+        buttonToggle:Dock(RIGHT)
+    end
+
+    if IsValid(buttonRun) then
+        buttonRun:SetParent(panel)
+        buttonRun:Dock(RIGHT)
     end
 
     if IsValid(right) then
@@ -105,21 +120,63 @@ function PANEL:Rebuild() end
 
 -- FUNCTIONS TO POPULATE THE FORM
 
-local function MakeReset(parent)
-    local reset = vgui.Create("DButtonTTT2", parent)
+local function MakeButton(parent)
+    local button = vgui.Create("DButtonTTT2", parent)
 
-    reset:SetText("button_default")
-    reset:SetSize(32, 32)
+    button:SetText("button_default")
+    button:SetSize(32, 32)
 
-    reset.Paint = function(slf, w, h)
+    button.Paint = function(slf, w, h)
         derma.SkinHook("Paint", "FormButtonIconTTT2", slf, w, h)
 
         return true
     end
 
-    reset.material = materialReset
+    return button
+end
 
-    return reset
+local function MakeResetButton(parent)
+    local buttonReset = MakeButton(parent)
+
+    buttonReset.iconMaterial = materialReset
+    buttonReset.roundedCorner = true
+
+    return buttonReset
+end
+
+local function MakeToggleButton(parent, data)
+    local buttonToggle = MakeButton(parent)
+
+    buttonToggle.state = data.toggleInitialState or 1
+    buttonToggle.iconMaterial = data.toggleIconMaterial or { materialCheckmark, materialCross }
+    buttonToggle.colorBackground = data.toggleColorBackground or { COLOR_OLIVE, COLOR_ORANGE }
+
+    buttonToggle.DoClick = function(slf)
+        slf.state = slf.state + 1
+
+        if slf.state > #slf.iconMaterial then
+            slf.state = 1
+        end
+
+        if isfunction(data.OnClickToggle) then
+            data.OnClickToggle(slf, slf.state)
+        end
+    end
+
+    return buttonToggle
+end
+
+local function MakeRunButton(parent, data)
+    local buttonRun = MakeButton(parent)
+
+    buttonRun.iconMaterial = data.runIconMaterial or materialRun
+    buttonRun.colorBackground = data.runColorBackground
+
+    if isfunction(data.OnClickRun) then
+        buttonRun.DoClick = data.OnClickRun
+    end
+
+    return buttonRun
 end
 
 ---
@@ -144,8 +201,19 @@ function PANEL:MakeTextEntry(data)
 
     local right = vgui.Create("DTextEntryTTT2", self)
 
-    local reset = MakeReset(self)
+    local reset = MakeResetButton(self)
     right:SetResetButton(reset)
+
+    -- optional buttons
+    local toggle, run
+
+    if data.enableToggle then
+        toggle = MakeToggleButton(self, data)
+    end
+
+    if data.enableRun then
+        run = MakeRunButton(self, data)
+    end
 
     right:SetUpdateOnType(false)
     right:SetHeightMult(1)
@@ -176,12 +244,22 @@ function PANEL:MakeTextEntry(data)
     right:SetTall(32)
     right:Dock(TOP)
 
-    self:AddItem(left, right, reset)
+    self:AddItem(left, right, reset, toggle, run)
 
     if IsValid(data.master) and isfunction(data.master.AddSlave) then
         data.master:AddSlave(left)
         data.master:AddSlave(right)
         data.master:AddSlave(reset)
+
+        if IsValid(toggle) then
+            toggle:SetMaster(data.master)
+            data.master:AddSlave(toggle)
+        end
+
+        if IsValid(run) then
+            run:SetMaster(data.master)
+            data.master:AddSlave(run)
+        end
     end
 
     return left, right
@@ -195,8 +273,19 @@ end
 function PANEL:MakeCheckBox(data)
     local left = vgui.Create("DCheckBoxLabelTTT2", self)
 
-    local reset = MakeReset(self)
+    local reset = MakeResetButton(self)
     left:SetResetButton(reset)
+
+    -- optional buttons
+    local toggle, run
+
+    if data.enableToggle then
+        toggle = MakeToggleButton(self, data)
+    end
+
+    if data.enableRun then
+        run = MakeRunButton(self, data)
+    end
 
     left:SetText(data.label)
     left:SetTextParams(data.params)
@@ -220,7 +309,7 @@ function PANEL:MakeCheckBox(data)
         end
     end
 
-    self:AddItem(left, nil, reset)
+    self:AddItem(left, nil, reset, toggle, run)
 
     if IsValid(data.master) and isfunction(data.master.AddSlave) then
         left:SetMaster(data.master)
@@ -228,6 +317,16 @@ function PANEL:MakeCheckBox(data)
 
         data.master:AddSlave(left)
         data.master:AddSlave(reset)
+
+        if IsValid(toggle) then
+            toggle:SetMaster(data.master)
+            data.master:AddSlave(toggle)
+        end
+
+        if IsValid(run) then
+            run:SetMaster(data.master)
+            data.master:AddSlave(run)
+        end
 
         left:DockMargin(left:GetIndentationMargin(), 0, 0, 0)
     end
@@ -253,8 +352,19 @@ function PANEL:MakeSlider(data)
 
     local right = vgui.Create("DNumSliderTTT2", self)
 
-    local reset = MakeReset(self)
+    local reset = MakeResetButton(self)
     right:SetResetButton(reset)
+
+    -- optional buttons
+    local toggle, run
+
+    if data.enableToggle then
+        toggle = MakeToggleButton(self, data)
+    end
+
+    if data.enableRun then
+        run = MakeRunButton(self, data)
+    end
 
     right:SetMinMax(data.min, data.max)
 
@@ -282,7 +392,7 @@ function PANEL:MakeSlider(data)
     right:SetTall(32)
     right:Dock(TOP)
 
-    self:AddItem(left, right, reset)
+    self:AddItem(left, right, reset, toggle, run)
 
     if IsValid(data.master) and isfunction(data.master.AddSlave) then
         data.master:AddSlave(left)
@@ -292,6 +402,64 @@ function PANEL:MakeSlider(data)
         left:SetMaster(data.master)
         right:SetMaster(data.master)
         reset:SetMaster(data.master)
+
+        if IsValid(toggle) then
+            toggle:SetMaster(data.master)
+            data.master:AddSlave(toggle)
+        end
+
+        if IsValid(run) then
+            run:SetMaster(data.master)
+            data.master:AddSlave(run)
+        end
+
+        left:DockMargin(left:GetIndentationMargin(), 0, 0, 0)
+    end
+
+    return left
+end
+
+---
+-- Adds a button
+-- @param table data The data for the slider
+-- @return Panel The created slider
+-- @realm client
+function PANEL:MakeButton(data)
+    local left = vgui.Create("DLabelTTT2", self)
+
+    left:SetText(data.label)
+
+    left.Paint = function(slf, w, h)
+        derma.SkinHook("Paint", "FormLabelTTT2", slf, w, h)
+
+        return true
+    end
+
+    local right = vgui.Create("DButtonTTT2", self)
+
+    right:SetText(data.buttonLabel)
+
+    if isfunction(data.OnClick) then
+        right.DoClick = data.OnClick
+    end
+
+    right:SetTall(32)
+    right:Dock(TOP)
+
+    right.Paint = function(slf, w, h)
+        derma.SkinHook("Paint", "FormButtonTTT2", slf, w, h)
+
+        return true
+    end
+
+    self:AddItem(left, right)
+
+    if IsValid(data.master) and isfunction(data.master.AddSlave) then
+        data.master:AddSlave(left)
+        data.master:AddSlave(right)
+
+        left:SetMaster(data.master)
+        right:SetMaster(data.master)
 
         left:DockMargin(left:GetIndentationMargin(), 0, 0, 0)
     end
@@ -324,8 +492,20 @@ function PANEL:MakeComboBox(data)
 
     local right = vgui.Create("DComboBoxTTT2", self)
 
-    local reset = MakeReset(self)
+    local reset = MakeResetButton(self)
     right:SetResetButton(reset)
+
+    -- optional buttons
+    local toggle, run
+
+    if data.enableToggle then
+        toggle = MakeToggleButton(self, data)
+    end
+
+    if data.enableRun then
+        run = MakeRunButton(self, data)
+    end
+
     right:SetDefaultValue(data.default) -- Set default if possible even if the convar could still overwrite it
 
     if data.choices then
@@ -369,7 +549,7 @@ function PANEL:MakeComboBox(data)
     right:SetTall(32)
     right:Dock(TOP)
 
-    self:AddItem(left, right, reset)
+    self:AddItem(left, right, reset, toggle, run)
 
     if IsValid(data.master) and isfunction(data.master.AddSlave) then
         data.master:AddSlave(left)
@@ -379,6 +559,16 @@ function PANEL:MakeComboBox(data)
         left:SetMaster(data.master)
         right:SetMaster(data.master)
         reset:SetMaster(data.master)
+
+        if IsValid(toggle) then
+            toggle:SetMaster(data.master)
+            data.master:AddSlave(toggle)
+        end
+
+        if IsValid(run) then
+            run:SetMaster(data.master)
+            data.master:AddSlave(run)
+        end
 
         left:DockMargin(left:GetIndentationMargin(), 0, 0, 0)
     end
@@ -424,7 +614,7 @@ function PANEL:MakeBinder(data)
     right:SetTall(32)
     right:Dock(TOP)
 
-    local reset = MakeReset(self)
+    local reset = MakeResetButton(self)
 
     if data.default ~= nil then
         reset.DoClick = function(slf)

gamemodes/terrortown/content/materials/vgui/ttt/vskin/icon_checkmark.vmt
@@ -1,0 +1,10 @@
+"UnlitGeneric"
+{
+	"$basetexture" "vgui/ttt/vskin/icon_checkmark"
+	"$nocull" 1
+	"$nodecal" 1
+	"$nolod" 1
+	"$translucent" 1
+	"$vertexalpha" 1
+	"$vertexcolor" 1
+}

gamemodes/terrortown/content/materials/vgui/ttt/vskin/icon_cross.vmt
@@ -1,0 +1,10 @@
+"UnlitGeneric"
+{
+	"$basetexture" "vgui/ttt/vskin/icon_cross"
+	"$nocull" 1
+	"$nodecal" 1
+	"$nolod" 1
+	"$translucent" 1
+	"$vertexalpha" 1
+	"$vertexcolor" 1
+}

gamemodes/terrortown/content/materials/vgui/ttt/vskin/icon_lock_closed.vmt
@@ -1,0 +1,10 @@
+"UnlitGeneric"
+{
+	"$basetexture" "vgui/ttt/vskin/icon_lock_closed"
+	"$nocull" 1
+	"$nodecal" 1
+	"$nolod" 1
+	"$translucent" 1
+	"$vertexalpha" 1
+	"$vertexcolor" 1
+}

commit ed8548b69e1f99a8a598f8a0fafbe793cb436b34

UI/Bindings: Added Run Button to Bindings Menu (#1516)
Added a run button to all bindings. When pressing the button, the
binding is executed, even if no key is added to that binding.


![image](https://github.com/TTT-2/TTT2/assets/13639408/08c43db4-4df6-4be4-bfd3-9f014e82525a)

It also fixes a small rendering issue introduced in #1509

gamemodes/terrortown/gamemode/client/cl_vskin/default_skin.lua
@@ -726,12 +726,6 @@ function SKIN:PaintFormButtonIconTTT2(panel, w, h)
         end
     end
 
-    local iconMaterial = panel.iconMaterial
-
-    if not iconMaterial.GetTexture then
-        iconMaterial = iconMaterial[panel.state or 1]
-    end
-
     local colorBoxBack = colors.settingsBox
     local colorText = ColorAlpha(utilGetDefaultColor(colors.accent), 150)
     local shift = 0
@@ -760,6 +754,16 @@ function SKIN:PaintFormButtonIconTTT2(panel, w, h)
     end
 
     drawRoundedBox(sizes.cornerRadius, 1, 1, w - 2, h - 2, colorBox)
+
+    local iconMaterial = panel.iconMaterial or panel.material
+
+    if not iconMaterial then
+        return
+    end
+
+    if not iconMaterial.GetTexture then
+        iconMaterial = iconMaterial[panel.state or 1]
+    end
 
     drawFilteredShadowedTexture(
         pad,

gamemodes/terrortown/gamemode/client/cl_vskin/vgui/dform_ttt2.lua
@@ -616,6 +616,17 @@ function PANEL:MakeBinder(data)
 
     local reset = MakeResetButton(self)
 
+    -- optional buttons
+    local toggle, run
+
+    if data.enableToggle then
+        toggle = MakeToggleButton(self, data)
+    end
+
+    if data.enableRun then
+        run = MakeRunButton(self, data)
+    end
+
     if data.default ~= nil then
         reset.DoClick = function(slf)
             right.binder:SetValue(data.default)
@@ -624,7 +635,7 @@ function PANEL:MakeBinder(data)
         reset.noDefault = true
     end
 
-    self:AddItem(left, right, reset)
+    self:AddItem(left, right, reset, toggle, run)
 
     if IsValid(data.master) and isfunction(data.master.AddSlave) then
         data.master:AddSlave(left)
@@ -634,6 +645,16 @@ function PANEL:MakeBinder(data)
         left:SetMaster(data.master)
         right:SetMaster(data.master)
         reset:SetMaster(data.master)
+
+        if IsValid(toggle) then
+            toggle:SetMaster(data.master)
+            data.master:AddSlave(toggle)
+        end
+
+        if IsValid(run) then
+            run:SetMaster(data.master)
+            data.master:AddSlave(run)
+        end
 
         left:DockMargin(left:GetIndentationMargin(), 0, 0, 0)
     end

lua/terrortown/menus/gamemode/bindings/bindings.lua
@@ -46,6 +46,25 @@ local function AddBindingCategory(category, parent)
 
                 currentBinding = keyNum
             end,
+            enableRun = true,
+            OnClickRun = function(button)
+                local bindingData = bind.registry[binding.name]
+
+                if not bindingData or not isfunction(bindingData.OnPressed) then
+                    return
+                end
+
+                bindingData.OnPressed()
+
+                -- run the release key binding a moment later
+                timer.Simple(0.1, function()
+                    if not bindingData or not isfunction(bindingData.OnReleased) then
+                        return
+                    end
+
+                    bindingData.OnReleased()
+                end)
+            end,
         })
     end
 end