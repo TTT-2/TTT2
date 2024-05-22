TTT-2/TTT2

commit f3f3852f1537f9a0ddcb7b2c8bc5850f60b45994

UI: Admin Commands (#1521)
I added basic admin commands to the UI to restart the round, set the
health etc


![image](https://github.com/TTT-2/TTT2/assets/13639408/e0267ec6-509b-4def-8b46-83ee423c8440)

---------

Co-authored-by: Histalek <16392835+Histalek@users.noreply.github.com>

gamemodes/terrortown/gamemode/client/cl_vskin/vgui/dcombobox_ttt2.lua
@@ -39,6 +39,36 @@ function PANEL:Init()
     self.valueIndices = {}
 
     self.valueType = nil
+
+    self.slaves = {}
+
+    local value, _ = self:GetSelected()
+
+    self:UpdateSlaves(IsValid(value))
+end
+
+---
+-- @param Panel slave
+-- @realm client
+function PANEL:AddSlave(slave)
+    if not IsValid(slave) then
+        return
+    end
+
+    self.slaves[#self.slaves + 1] = slave
+
+    local value, _ = self:GetSelected()
+
+    slave:SetEnabled(IsValid(value))
+end
+
+---
+-- @param boolean val
+-- @realm client
+function PANEL:UpdateSlaves(val)
+    for i = 1, #self.slaves do
+        self.slaves[i]:SetEnabled(val)
+    end
 end
 
 ---
@@ -218,6 +248,8 @@ function PANEL:ChooseOptionID(index, ignoreCallbackEnabledVars)
 
     self:SetText(choice.title)
     self:OnSelect(index, value, choice.data)
+
+    self:UpdateSlaves(IsValid(value))
 
     self:CloseMenu()
 

gamemodes/terrortown/gamemode/client/cl_vskin/default_skin.lua
@@ -792,12 +792,10 @@ function SKIN:PaintFormButtonTTT2(panel, w, h)
         colorBox = ColorAlpha(colors.accent, alphaDisabled)
         colorText = ColorAlpha(utilGetDefaultColor(colors.accent), alphaDisabled)
     elseif panel.Depressed or panel:IsSelected() or panel:GetToggle() then
-        colorBoxBack = colors.settingsBox
         colorBox = colors.accentActive
         colorText = ColorAlpha(utilGetDefaultColor(colors.accent), 150)
         shift = 1
     elseif panel.Hovered then
-        colorBoxBack = colors.settingsBox
         colorBox = colors.accentHover
         colorText = ColorAlpha(utilGetDefaultColor(colors.accent), 150)
     end

gamemodes/terrortown/gamemode/client/cl_vskin/vgui/dform_ttt2.lua
@@ -374,6 +374,7 @@ function PANEL:MakeSlider(data)
 
     -- Set default if possible even if the convar could still overwrite it
     right:SetDefaultValue(data.default)
+    right:SetValue(data.initial)
     right:SetConVar(data.convar)
     right:SetServerConVar(data.serverConvar)
     right:SetDatabase(data.database)
@@ -416,7 +417,7 @@ function PANEL:MakeSlider(data)
         left:DockMargin(left:GetIndentationMargin(), 0, 0, 0)
     end
 
-    return left
+    return right, left
 end
 
 ---
@@ -464,7 +465,7 @@ function PANEL:MakeButton(data)
         left:DockMargin(left:GetIndentationMargin(), 0, 0, 0)
     end
 
-    return left
+    return right, left
 end
 
 ---

gamemodes/terrortown/gamemode/server/sv_main.lua
@@ -955,7 +955,10 @@ local function CleanUp()
     hook.Remove("PlayerSay", "ULXMeCheck")
 end
 
-local function StopRoundTimers()
+---
+-- Stops the timers in order to restart a round.
+-- @realm server
+function StopRoundTimers()
     -- remove all timers
     timer.Stop("wait2prep")
     timer.Stop("prep2begin")

gamemodes/terrortown/gamemode/shared/sh_init.lua
@@ -564,6 +564,7 @@ include("ttt2/extensions/chat.lua")
 include("ttt2/extensions/sound.lua")
 
 -- include libraries
+include("ttt2/libraries/admin.lua")
 include("ttt2/libraries/map.lua")
 include("ttt2/libraries/none.lua")
 include("ttt2/libraries/fastutf8.lua")

lua/terrortown/lang/en.lua
@@ -2262,3 +2262,29 @@ L.header_maps_prefixes = "Enable/Disable Maps by their Prefix"
 L.header_maps_select = "Select and Change Maps"
 
 L.button_change_map = "Change Map"
+
+-- 2024-05-20
+L.submenu_commands_commands_title = "Commands"
+
+L.header_commands_round_restart = "Round Restart"
+L.header_commands_player_slay = "Slay Player"
+L.header_commands_player_teleport = "Teleport Player to Focused Point"
+L.header_commands_player_respawn = "Respawn Player at Focused Point"
+L.header_commands_player_add_credits = "Add Equipment Credits"
+L.header_commands_player_set_health = "Set Health"
+L.header_commands_player_set_armor = "Set Armor"
+
+L.label_button_round_restart = "round restart"
+L.label_button_player_slay = "slay player"
+L.label_button_player_teleport = "teleport player"
+L.label_button_player_respawn = "respawn player"
+L.label_button_player_add_credits = "add credits"
+L.label_button_player_set_health = "set health"
+L.label_button_player_set_armor = "set armor"
+
+L.label_slider_add_credits = "Set credit amount"
+L.label_slider_set_health = "Set health"
+L.label_slider_set_armor = "Set armor"
+
+L.label_player_select = "Select affected player"
+L.label_execute_command = "Execute command"

lua/terrortown/menus/gamemode/commands/commands.lua
@@ -1,0 +1,179 @@
+--- @ignore
+
+CLGAMEMODESUBMENU.base = "base_gamemodesubmenu"
+
+CLGAMEMODESUBMENU.priority = 99
+CLGAMEMODESUBMENU.title = "submenu_commands_commands_title"
+
+function CLGAMEMODESUBMENU:Populate(parent)
+    local plys = player.GetAll()
+    local plyChoices = {}
+
+    for i = 1, #plys do
+        local ply = plys[i]
+
+        plyChoices[i] = {
+            title = ply:Nick(),
+            value = ply,
+        }
+    end
+
+    -- RESTART ROUND --
+
+    local form = vgui.CreateTTT2Form(parent, "header_commands_round_restart")
+
+    form:MakeButton({
+        label = "label_execute_command",
+        buttonLabel = "label_button_round_restart",
+        OnClick = function(slf)
+            admin.RoundRestart()
+        end,
+    })
+
+    -- SLAY PLAYER --
+
+    local form2 = vgui.CreateTTT2Form(parent, "header_commands_player_slay")
+
+    local playerSlay = form2:MakeComboBox({
+        label = "label_player_select",
+        choices = plyChoices,
+    })
+
+    form2:MakeButton({
+        label = "label_execute_command",
+        buttonLabel = "label_button_player_slay",
+        OnClick = function(slf)
+            local ply, _ = playerSlay:GetSelected()
+
+            admin.PlayerSlay(ply)
+        end,
+        master = playerSlay,
+    })
+
+    -- TELEPORT PLAYER --
+
+    local form3 = vgui.CreateTTT2Form(parent, "header_commands_player_teleport")
+
+    local playerTeleport = form3:MakeComboBox({
+        label = "label_player_select",
+        choices = plyChoices,
+    })
+
+    form3:MakeButton({
+        label = "label_execute_command",
+        buttonLabel = "label_button_player_teleport",
+        OnClick = function(slf)
+            local ply, _ = playerTeleport:GetSelected()
+
+            admin.PlayerTeleport(ply, LocalPlayer():GetEyeTrace().HitPos)
+        end,
+        master = playerTeleport,
+    })
+
+    -- RESPAWN PLAYER --
+
+    local form4 = vgui.CreateTTT2Form(parent, "header_commands_player_respawn")
+
+    local playerRespawn = form4:MakeComboBox({
+        label = "label_player_select",
+        choices = plyChoices,
+    })
+
+    form4:MakeButton({
+        label = "label_execute_command",
+        buttonLabel = "label_button_player_respawn",
+        OnClick = function(slf)
+            local ply, _ = playerRespawn:GetSelected()
+
+            admin.PlayerRespawn(ply, LocalPlayer():GetEyeTrace().HitPos)
+        end,
+        master = playerRespawn,
+    })
+
+    -- ADD CREDITS --
+
+    local form5 = vgui.CreateTTT2Form(parent, "header_commands_player_add_credits")
+
+    local playerCredits = form5:MakeComboBox({
+        label = "label_player_select",
+        choices = plyChoices,
+    })
+
+    local amountCredits = form5:MakeSlider({
+        label = "label_slider_add_credits",
+        min = 0,
+        max = 25,
+        decimal = 0,
+        initial = 2,
+        master = playerCredits,
+    })
+
+    form5:MakeButton({
+        label = "label_execute_command",
+        buttonLabel = "label_button_player_add_credits",
+        OnClick = function(slf)
+            local ply, _ = playerCredits:GetSelected()
+
+            admin.PlayerAddCredits(ply, amountCredits:GetValue())
+        end,
+        master = playerCredits,
+    })
+
+    -- SET HEALTH --
+
+    local form6 = vgui.CreateTTT2Form(parent, "header_commands_player_set_health")
+
+    local playerHealth = form6:MakeComboBox({
+        label = "label_player_select",
+        choices = plyChoices,
+    })
+
+    local amountHealth = form6:MakeSlider({
+        label = "label_slider_set_health",
+        min = 0,
+        max = 200,
+        decimal = 0,
+        initial = 100,
+        master = playerHealth,
+    })
+
+    form6:MakeButton({
+        label = "label_execute_command",
+        buttonLabel = "label_button_player_set_health",
+        OnClick = function(slf)
+            local ply, _ = playerHealth:GetSelected()
+
+            admin.PlayerSetHealth(ply, amountHealth:GetValue())
+        end,
+        master = playerHealth,
+    })
+
+    -- SET ARMOR --
+
+    local form7 = vgui.CreateTTT2Form(parent, "header_commands_player_set_armor")
+
+    local playerArmor = form7:MakeComboBox({
+        label = "label_player_select",
+        choices = plyChoices,
+    })
+
+    local amountArmor = form7:MakeSlider({
+        label = "label_slider_set_armor",
+        min = 0,
+        max = 100,
+        decimal = 0,
+        initial = 30,
+        master = playerArmor,
+    })
+
+    form7:MakeButton({
+        label = "label_execute_command",
+        buttonLabel = "label_button_player_set_armor",
+        OnClick = function(slf)
+            local ply, _ = playerArmor:GetSelected()
+
+            admin.PlayerSetArmor(ply, amountArmor:GetValue())
+        end,
+        master = playerArmor,
+    })
+end

lua/terrortown/menus/gamemode/commands/maps.lua
@@ -2,7 +2,7 @@
 
 CLGAMEMODESUBMENU.base = "base_gamemodesubmenu"
 
-CLGAMEMODESUBMENU.priority = 100
+CLGAMEMODESUBMENU.priority = 98
 CLGAMEMODESUBMENU.title = "submenu_commands_maps_title"
 
 local comboBase

commit b3a0491449388bf4e2c22a304cea015636ecf180

Fix: Combobox Validity Check (#1526)
Fixed an issue introduced by #1521

This happens because combo boxes can have values where the validity
check fails (like strings).

gamemodes/terrortown/gamemode/client/cl_vskin/vgui/dcombobox_ttt2.lua
@@ -44,7 +44,7 @@ function PANEL:Init()
 
     local value, _ = self:GetSelected()
 
-    self:UpdateSlaves(IsValid(value))
+    self:UpdateSlaves(value ~= nil)
 end
 
 ---
@@ -59,7 +59,7 @@ function PANEL:AddSlave(slave)
 
     local value, _ = self:GetSelected()
 
-    slave:SetEnabled(IsValid(value))
+    slave:SetEnabled(value ~= nil)
 end
 
 ---
@@ -249,7 +249,7 @@ function PANEL:ChooseOptionID(index, ignoreCallbackEnabledVars)
     self:SetText(choice.title)
     self:OnSelect(index, value, choice.data)
 
-    self:UpdateSlaves(IsValid(value))
+    self:UpdateSlaves(value ~= nil)
 
     self:CloseMenu()
 