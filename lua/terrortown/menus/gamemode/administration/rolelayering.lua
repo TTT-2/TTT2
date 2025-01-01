c6c0f1d0a5bec5fe1cc832b17e8192b68df02aeb
DaNike <danike@cirr.com>
2025-01-01 01:54:42 -0600

    Re-add administration submenu files to override the Workshop files

---
+++ lua/terrortown/menus/gamemode/administration/rolelayering.lua
@@ -0,0 +1,16 @@
+--- @ignore
+
+-- This file exists to override the file in the workshop version if it still has it here.
+-- Accordingly, it can be removed for the workshop release.
+
+CLGAMEMODESUBMENU.base = "base_gamemodesubmenu"
+
+CLGAMEMODESUBMENU.priority = 97
+CLGAMEMODESUBMENU.title = "submenu_administration_rolelayering_title"
+
+local TryT = LANG.TryTranslation
+
+function CLGAMEMODESUBMENU:Populate(parent)
+end
+
+function CLGAMEMODESUBMENU:ShouldShow() return false end
