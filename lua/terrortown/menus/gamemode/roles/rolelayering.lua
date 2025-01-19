d56fc6270e6a010c775f4460770cc889bc40b886
Tim Goll <github@timgoll.de>
2025-01-19 13:41:54 +0100

    added server convar to moved file

--- lua/terrortown/menus/gamemode/roles/rolelayering.lua
+++ lua/terrortown/menus/gamemode/roles/rolelayering.lua
@@ -148,6 +148,7 @@ hook.Add("TTT2ReceivedRolelayerData", "received_layer_data", function(role, laye
         ic:SetColor(roleData.color)
         ic:SetTooltip(roleData.name)
         ic:SetTooltipFixedPosition(0, 64)
+        ic:SetServerConVar("ttt_" .. roleData.name .. "_enabled")
 
         ic.subrole = subrole
 
