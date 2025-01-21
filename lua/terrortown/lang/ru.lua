77e9d7f210166b4ec42431ea8e934d0e434192a6
TimGoll <13639408+TimGoll@users.noreply.github.com>
2025-01-21 08:40:11 +0000

    Update language files

--- lua/terrortown/lang/ru.lua
+++ lua/terrortown/lang/ru.lua
@@ -2399,6 +2399,7 @@ L.corpse_hint_without_confirm = "[{usekey}] Осмотреть тело."
 --L.label_menu_search_no_items = "No items matched your search."
 
 --L.submenu_roles_overview_title = "Roles Overview (READ ME)"
+
 -- Is there a way to ahve some sort of external file that's possibly-localized?
 --L.roles_overview_html = [[
 --<h1>Overview</h1>
@@ -2687,3 +2688,6 @@ L.corpse_hint_without_confirm = "[{usekey}] Осмотреть тело."
 
 -- 2025-01-19
 --L.help_rolelayering_enable = "The red and green border around the icon shows if the role is currently enabled. Right click on an icon to quickly enable/disable that role."
+
+-- 2025-01-20
+--L.label_hud_show_team_name = "Enable showing team name next to role name"
