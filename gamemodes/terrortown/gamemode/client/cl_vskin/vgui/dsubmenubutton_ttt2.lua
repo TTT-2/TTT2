252eee65d1d3d910ca694425d0fc297f3af06f5d
Tim Goll <github@timgoll.de>
2025-01-31 08:58:44 +0100

    started with TTT2:DSubmenuButton

--- gamemodes/terrortown/gamemode/client/cl_vskin/vgui/dsubmenubutton_ttt2.lua
+++ gamemodes/terrortown/gamemode/client/cl_vskin/vgui/dsubmenubutton_ttt2.lua
@@ -5,41 +5,9 @@
 local PANEL = {}
 
 ---
--- @accessor boolean
--- @realm client
-AccessorFunc(PANEL, "m_bBorder", "DrawBorder", FORCE_BOOL)
-
----
 -- @ignore
 function PANEL:Init()
-    self:SetContentAlignment(5)
-
-    self:SetDrawBorder(true)
-    self:SetPaintBackground(true)
-
-    self:SetTall(22)
-    self:SetMouseInputEnabled(true)
-    self:SetKeyboardInputEnabled(true)
-
-    self:SetCursor("hand")
-    self:SetText("")
-
-    self.contents = {
-        title = "",
-        title_font = "DermaTTT2SubMenuButtonTitle",
-        icon = nil,
-        iconFullSize = false,
-        iconBadge = nil,
-        iconBadgeSize = 0,
-        selected = false,
-    }
-end
-
----
--- @return boolean
--- @realm client
-function PANEL:IsDown()
-    return self.Depressed
+    self:SetPaintHookName("SubMenuButtonTTT2")
 end
 
 ---
@@ -150,28 +118,9 @@ function PANEL:Paint(w, h)
     return false
 end
 
----
--- @ignore
-function PANEL:PerformLayout()
-    DLabel.PerformLayout(self)
-end
-
----
--- @param string strName
--- @param string strArgs
--- @realm client
-function PANEL:SetConsoleCommand(strName, strArgs)
-    self.DoClick = function(slf, val)
-        RunConsoleCommand(strName, strArgs)
-    end
-end
-
----
--- @ignore
-function PANEL:SizeToContents()
-    local w, h = self:GetContentSize()
-
-    self:SetSize(w + 8, h + 4)
-end
-
-derma.DefineControl("DSubmenuButtonTTT2", "A standard Button", PANEL, "TTT2:DButton")
+derma.DefineControl(
+    "TTT2:DSubmenuButton",
+    "A spin on TTT2's button that has some extra features for the submenu list",
+    PANEL,
+    "TTT2:DButton"
+)
