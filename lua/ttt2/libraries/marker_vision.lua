TTT-2/TTT2

commit 92bf9e0d4eb2d14b76b323c2fb701c02b99eef7b

Markervision: Added UpdateTransmitState handling (#1825)
This will now handle UpdateTransmitState for markervision entities.

Please give me all feedback you have.. Right now, this is the easiest
solution I was able to come up with.

(will add changelog later)

---------

Co-authored-by: Tim Goll <github@timgoll.de>

lua/ttt2/libraries/marker_vision.lua
@@ -43,6 +43,16 @@ function markerVision.Add(ent, identifier)
 
     markerVision.registry[#markerVision.registry + 1] = mvObject
 
+    -- Overwrite the entity's UpdateTransmitState function to TRANSMIT_ALWAYS.
+    -- This is necessary because otherwise the entity might get culled.
+    -- Before we overwrite the function, we save the old function to restore it later.
+    ent.ttt2MVTransmitOldFunc = ent.UpdateTransmitState
+
+    ent.UpdateTransmitState = function()
+        return TRANSMIT_ALWAYS
+    end
+    ent:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
+
     return mvObject
 end
 
@@ -92,6 +102,11 @@ function markerVision.Remove(ent, identifier)
     if CLIENT then
         marks.Remove({ ent })
     end
+
+    -- Restore the old value for UpdateTransmitState and clear ttt2MVTransmitOldFunc.
+    ent.UpdateTransmitState = ent.ttt2MVTransmitOldFunc
+    ent:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
+    ent.ttt2MVTransmitOldFunc = nil
 end
 
 local entmeta = assert(FindMetaTable("Entity"), "[TTT2] FAILED TO FIND ENTITY TABLE")

commit d92d83e64bbea1d6bd1c022145773814ee5e3607

Markervision: Fixed null entity error (#1835)
This PR fixes #1834 .
The null entity error was introduced in #1825 .

I tested it and I couldn't reproduce the error from the above issue
again.

Error reason:
markerVision.Remove is called twice.
The first time ent is valid but the second time it's a null entity and
that caused the error.

lua/ttt2/libraries/marker_vision.lua
@@ -104,9 +104,11 @@ function markerVision.Remove(ent, identifier)
     end
 
     -- Restore the old value for UpdateTransmitState and clear ttt2MVTransmitOldFunc.
-    ent.UpdateTransmitState = ent.ttt2MVTransmitOldFunc
-    ent:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
-    ent.ttt2MVTransmitOldFunc = nil
+    if IsValid(ent) then
+        ent.UpdateTransmitState = ent.ttt2MVTransmitOldFunc
+        ent:RemoveEFlags(EFL_FORCE_CHECK_TRANSMIT)
+        ent.ttt2MVTransmitOldFunc = nil
+    end
 end
 
 local entmeta = assert(FindMetaTable("Entity"), "[TTT2] FAILED TO FIND ENTITY TABLE")