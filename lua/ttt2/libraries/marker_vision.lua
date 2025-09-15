TTT-2/TTT2

commit 92bf9e0d4eb2d14b76b323c2fb701c02b99eef7b

Markervision: Added UpdateTransmitState handling (#1825)
This will now handle UpdateTransmitState for markervision entities.

Please give me all feedback you have.. Right now, this is the easiest
solution I was able to come up with.

(will add changelog later)

---------

Co-authored-by: Tim Goll <github@timgoll.de>

lua/ttt2/libraries/marker_vision.lua
@@ -43,6 +43,16 @@ function markerVision.Add(ent, identifier)
 
     markerVision.registry[#markerVision.registry + 1] = mvObject
 
+    -- Overwrite the entity's UpdateTransmitState function to TRANSMIT_ALWAYS.
+    -- This is necessary because otherwise the entity might get culled.
+    -- Before we overwrite the function, we save the old function to restore it later.
+    ent.ttt2MVTransmitOldFunc = ent.UpdateTransmitState
+
+    ent.UpdateTransmitState = function()
+        return TRANSMIT_ALWAYS
+    end
+    ent:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
+
     return mvObject
 end
 
@@ -92,6 +102,11 @@ function markerVision.Remove(ent, identifier)
     if CLIENT then
         marks.Remove({ ent })
     end
+
+    -- Restore the old value for UpdateTransmitState and clear ttt2MVTransmitOldFunc.
+    ent.UpdateTransmitState = ent.ttt2MVTransmitOldFunc
+    ent:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
+    ent.ttt2MVTransmitOldFunc = nil
 end
 
 local entmeta = assert(FindMetaTable("Entity"), "[TTT2] FAILED TO FIND ENTITY TABLE")

commit d391d48024dbb43a825e10eb7b185570995475c3

Markervision: Fixed another null entity error (#1837)
This PR fixes the following error:
```
[REDACTED] Lua Error:

[TTT2 (Base) - v0.14.5b] lua/ttt2/libraries/marker_vision.lua:54: Tried to use a NULL entity!
  1. AddEFlags - [C]:-1
   2. Add - lua/ttt2/libraries/marker_vision.lua:54
    3. callback - lua/ttt2/libraries/marker_vision.lua:230
     4. func - lua/ttt2/extensions/net.lua:206
      5. unknown - lua/includes/extensions/net.lua:34
```

The null entity error was introduced in
https://github.com/TTT-2/TTT2/pull/1825 .

I tested it and I couldn't reproduce the above error again.

(I fixed a similar error in the following PR but I didn't notice the
other error: https://github.com/TTT-2/TTT2/pull/1835 )

lua/ttt2/libraries/marker_vision.lua
@@ -46,12 +46,14 @@ function markerVision.Add(ent, identifier)
     -- Overwrite the entity's UpdateTransmitState function to TRANSMIT_ALWAYS.
     -- This is necessary because otherwise the entity might get culled.
     -- Before we overwrite the function, we save the old function to restore it later.
-    ent.ttt2MVTransmitOldFunc = ent.UpdateTransmitState
+    if IsValid(ent) then
+        ent.ttt2MVTransmitOldFunc = ent.UpdateTransmitState
 
-    ent.UpdateTransmitState = function()
-        return TRANSMIT_ALWAYS
+        ent.UpdateTransmitState = function()
+            return TRANSMIT_ALWAYS
+        end
+        ent:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
     end
-    ent:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
 
     return mvObject
 end